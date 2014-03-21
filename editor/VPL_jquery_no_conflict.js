bbca9b8660533f4bb1de5529eb49554879da35c1
Juan Carlos Rodriguez-del-Pino <jcrodriguez@dis.ulpgc.es>
2014-03-21 15:15:54 +0000

    Get global var $JQVPL from jQuery.noConflict(true), must by load after jQuery

---
+++ editor/VPL_jquery_no_conflict.js
@@ -0,0 +1,10 @@
+/**
+ * @version $Id: WCodeEditor.js,v 1. 2012-10-05 09:03:48 juanca Exp $
+ * @package VPL. HTML/JavaScript Code Editor
+ * @copyright 2013 Juan Carlos Rodríguez-del-Pino
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
+ */
+//Get correct version of jQuery for use without conflict in VPL
+//This must be run after loading VPL jQuery
+$JQVPL= jQuery.noConflict(true);
