958242d24683b1b95595d32891f231516a09472b
Juan Carlos Rodriguez-del-Pino <jcrodriguez@dis.ulpgc.es>
2014-06-18 11:52:09 +0100

    Update version

--- version.php
+++ version.php
@@ -1,8 +1,8 @@
 <?php // $Id: version.php,v 1.22 2013-07-11 16:47:04 juanca Exp $
 
 defined('MOODLE_INTERNAL') || die();
-
-$module->version = 2014052812;	//Current module version 3.1
+//TODO Change module to plugin NOT changed for incompatibility with previous versions
+$module->version = 2014052912;	//Current module version 3.1
 $module->cron    = 300; 		//cron check this module every 5 minutes
 $module->requires = 2012062500;
 $module->maturity = MATURITY_STABLE;
