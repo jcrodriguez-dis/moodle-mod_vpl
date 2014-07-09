ac0a2f774314576c26cb2013fbd419c7aca7eba9
Juan Carlos Rodriguez-del-Pino <jcrodriguez@dis.ulpgc.es>
2014-07-09 13:48:30 +0100

    Adapted to moodle >= 2.6

--- version.php
+++ version.php
@@ -4,7 +4,7 @@ defined('MOODLE_INTERNAL') || die();
 //TODO Change module to plugin NOT changed for incompatibility with previous versions
 $plugin->version = 2014052912;	//Current module version 3.1
 $plugin->cron    = 300; 		//cron check this module every 5 minutes
-$plugin->requires = 2012062500;
+$plugin->requires = 2013101800;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '3.1';
 $plugin->component = 'mod_vpl';
