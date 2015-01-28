46fffb3e9bfa1e9d19ad01babaeed60c5a7f491e
Juan Carlos Rodriguez-del-Pino <jcrodriguez@dis.ulpgc.es>
2015-01-28 12:53:19 +0000

    Updated version number

--- version.php
+++ version.php
@@ -1,10 +1,10 @@
-<?php // $Id: version.php,v 1.22 2013-07-11 16:47:04 juanca Exp $
+<?php // @copyright 2014 onward Juan Carlos Rodríguez-del-Pino
+
 
 defined('MOODLE_INTERNAL') || die();
-//TODO Change module to plugin NOT changed for incompatibility with previous versions
-$plugin->version = 2014052912;	//Current module version 3.1
-$plugin->cron    = 300; 		//cron check this module every 5 minutes
+$plugin->version = 2014052912;
+$plugin->cron    = 300; 		//cron check this plugin every 5 minutes
 $plugin->requires = 2013101800;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.1';
+$plugin->release = '3.2';
 $plugin->component = 'mod_vpl';
