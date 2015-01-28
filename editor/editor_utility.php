a81af0d8a7ffb43c2c68fc01651e3606e74746b8
Juan Carlos Rodriguez-del-Pino <jcrodriguez@dis.ulpgc.es>
2015-01-28 09:31:30 +0000

    Added new Javascript libraries Added new IDE features Added new i18n strings

--- editor/editor_utility.php
+++ editor/editor_utility.php
@@ -10,36 +10,45 @@
 /**
  * Class with auxiliar functions for the editor
  **/
+
 class vpl_editor_util{
 	static function generate_jquery(){
 		global $PAGE;
-		//REMOVE this comment to use Moodle jquery version
+		$PAGE->requires->css(new moodle_url('/mod/vpl/editor/font-awesome/css/font-awesome.min.css'));
 		//Auto selection dont work in 2.3
-		/*if(method_exists($PAGE->requires,'jquery_plugin')){
+		if(method_exists($PAGE->requires,'jquery_plugin')){
 			$PAGE->requires->jquery();
 			$PAGE->requires->jquery_plugin('ui');
 			$PAGE->requires->jquery_plugin('ui-css');
-		}else*/{
+			$PAGE->requires->js(new moodle_url('/mod/vpl/editor/jquery-touch/jquery.ui.touch-punch.min.js'),true);
+		}else{
 			$PAGE->requires->css(new moodle_url('/mod/vpl/editor/jquery/themes/smoothness/jquery-ui.css'));
 			$PAGE->requires->js(new moodle_url('/mod/vpl/editor/jquery/jquery-1.9.1.js'),true);
 			$PAGE->requires->js(new moodle_url('/mod/vpl/editor/jquery/jquery-ui-1.10.3.custom.js'),true);
-			$PAGE->requires->js(new moodle_url('/mod/vpl/editor/VPL_jquery_no_conflict.js'),true);
+			$PAGE->requires->js(new moodle_url('/mod/vpl/editor/jquery-touch/jquery.ui.touch-punch.min.js'),true);
 		}
+		$PAGE->requires->js(new moodle_url('/mod/vpl/editor/VPL_jquery_no_conflict.js'),true);
 	}
 	static function generate_requires_evaluation(){
 		global $PAGE;
 		self::generate_jquery();
 		$PAGE->requires->css(new moodle_url('/mod/vpl/editor/VPLIDE.css'));
+		$PAGE->requires->js(new moodle_url('/mod/vpl/editor/VPLUtil.js'),true);
 		$PAGE->requires->js(new moodle_url('/mod/vpl/editor/evaluationMonitor.js'),true);
 	}
 	static function generate_requires(){
 		global $PAGE;
 		self::generate_jquery();
-		$PAGE->requires->js(new moodle_url('/mod/vpl/editor/ace9/ace.js'));
-		$PAGE->requires->js(new moodle_url('/mod/vpl/editor/term.js'));
 		$PAGE->requires->css(new moodle_url('/mod/vpl/editor/VPLIDE.css'));
+		$PAGE->requires->js(new moodle_url('/mod/vpl/editor/zip/inflate.js'));
+		$PAGE->requires->js(new moodle_url('/mod/vpl/editor/zip/unzip.js'));
+		$PAGE->requires->js(new moodle_url('/mod/vpl/editor/ace9/ace.js'));
+		$PAGE->requires->js(new moodle_url('/mod/vpl/editor/ace9/ext-language_tools.js'));
+		$PAGE->requires->js(new moodle_url('/mod/vpl/editor/term.js'));
+		$PAGE->requires->js(new moodle_url('/mod/vpl/editor/VPLUtil.js'));
 		$PAGE->requires->js(new moodle_url('/mod/vpl/editor/VPLTerminal.js'));
 		$PAGE->requires->js(new moodle_url('/mod/vpl/editor/VPLIDE.js'));
+		$PAGE->requires->js(new moodle_url('/mod/vpl/editor/VPLIDEFile.js'));
 		$PAGE->requires->js(new moodle_url('/mod/vpl/editor/noVNC/include/util.js'),true);
 	}
 static function print_tag($options,$files_to_send,$saved=true){
@@ -49,7 +58,7 @@ static function print_tag($options,$files_to_send,$saved=true){
 	foreach($files_to_send as $name => $data){
 		$file = new stdClass();
 		$file->name=$name;
-		$file->data=$data;
+		$file->data=vpl_encode_binary($name,$data);
 		$files[] = $file;
 	}
 	$options['files']=$files;
@@ -58,12 +67,20 @@ static function print_tag($options,$files_to_send,$saved=true){
 	$joptions=json_encode($options);
 ?>
 <div id="<?php echo $tag_id;?>" class="vpl_ide vpl_ide_root ui-widget">
-	<ul id="vpl_menu" class="vpl_ide_menu"></ul>
+	<div id="vpl_menu" class="vpl_ide_menu"></div>
 	<div id="vpl_tr" class="vpl_ide_tr">
-		<div id="vpl_tabs" class="vpl_ide_tabs">
-			<ul id="vpl_tabs_lu"></ul>
+		<div id="vpl_filelist" style="display:none;">
+			<div id="vpl_filelist_header"><?php p(get_string('filelist',VPL))?></div>
+			<div id="vpl_filelist_content"></div>
 		</div>
-		<div id="vpl_results" class="vpl_ide_results"></div>
+		<div id="vpl_tabs" class="vpl_ide_tabs">
+			<div id="vpl_tabs_scroll">
+				<ul id="vpl_tabs_ul"></ul>
+			</div>
+		</div>
+		<div id="vpl_results" class="vpl_ide_results">
+		 <div id="vpl_results_accordion"></div>
+		</div>
 	</div>
 	<div id="vpl_ide_dialog_new" class="vpl_ide_dialog"
 		style="display: none;">
@@ -72,7 +89,7 @@ static function print_tag($options,$files_to_send,$saved=true){
 				<?php p(get_string('new_file_name',VPL))?></label> <input
 				type="text" id="vpl_ide_input_newfilename"
 				name="vpl_ide_input_newfilename" value=""
-				class="ui-widget-content ui-corner-all" /><br />
+				class="ui-widget-content ui-corner-all" autofocus /><br />
 		</fieldset>
 	</div>
 	<div id="vpl_ide_dialog_rename" class="vpl_ide_dialog"
@@ -82,7 +99,7 @@ static function print_tag($options,$files_to_send,$saved=true){
 				<?php p(get_string('rename'))?></label> <input type="text"
 				id="vpl_ide_input_renamefilename"
 				name="vpl_ide_input_renamefilename" value=""
-				class="ui-widget-content ui-corner-all" /><br />
+				class="ui-widget-content ui-corner-all" autofocus /><br />
 		</fieldset>
 	</div>
 	<div id="vpl_ide_dialog_delete" class="vpl_ide_dialog"
@@ -92,8 +109,12 @@ static function print_tag($options,$files_to_send,$saved=true){
 				<?php p(get_string('delete'))?></label> <input type="text"
 				id="vpl_ide_input_deletefilename"
 				name="vpl_ide_input_deletefilename" value=""
-				class="ui-widget-content ui-corner-all" /><br />
+				class="ui-widget-content ui-corner-all" autofocus /><br />
 		</fieldset>
+	</div>
+	<div id="vpl_ide_dialog_sort" class="vpl_ide_dialog"
+		style="display: none;">
+		<ol id="vpl_sort_list"></ol>
 	</div>
 	<div id="vpl_ide_dialog_about" class="vpl_ide_dialog"
 		style="display: none;">
@@ -117,23 +138,24 @@ static function print_tag($options,$files_to_send,$saved=true){
 			<li><a href="http://http://jquery.com/" target="_blank">jQuery and JQuery-ui</a>: jQuery is a fast, small, and feature-rich JavaScript library. Copyright The jQuery Foundation. (<a href="../editor/jquery/MIT-LICENSE.txt">licence</a>)</li>
 				</ul>
 	</div>
-	<div style="display: none;">
+	<form style="display:none;">
 		<input type="file" multiple="multiple" id="vpl_ide_input_file" />
-	</div>
+	</form>
 	<div id="vpl_dialog_terminal">
+		<div style="display: none;"><input type="text" id="vpl_input" /></div>
 		<pre id="vpl_terminal" class="vpl_terminal"></pre>
 	</div>
 	<div id="vpl_dialog_vnc">
-		<canvas class="noVNC_canvas">
+		<canvas class="vpl_noVNC_canvas">
                 Canvas not supported.
-            </canvas>
+         </canvas>
 	</div>
 </div>
 <script>
 	INCLUDE_URI="../editor/noVNC/include/";
     Util.load_scripts(["webutil.js", "base64.js", "websock.js", "des.js",
                        "keysymdef.js", "keyboard.js", "input.js", "display.js",
-                       "jsunzip.js", "rfb.js"]);
+                       "jsunzip.js", "rfb.js", "keysym.js"]);
 	$JQVPL(document).ready(function(){
 		$JQVPL("#page-footer").hide();
 		vpl_ide = new VPL_IDE('<?php echo $tag_id;?>',<?php echo $joptions;?>);
@@ -151,11 +173,11 @@ static function send_CE($CE) {
 	 * get list of i18n translations for the editor
 	 **/
 	static function i18n() {
-		global $PAGE;
 		$vpl_words = array (
 'about',
 'acceptcertificates',
 'acceptcertificatesnote',
+'binaryfile',
 'browserupdate',
 'changesNotSaved',
 'comments',
@@ -180,6 +202,7 @@ static function send_CE($CE) {
 'execution',
 'getjails',
 'file',
+'filelist',
 'filenotadded',
 'filenotdeleted',
 'filenotrenamed',
@@ -210,12 +233,14 @@ static function send_CE($CE) {
 		);
 		$words = array (
 'cancel',
+'closebuttontitle',
 'error',
 'import',
 'modified',
 'notice',
 'ok',
 'required',
+'sort',
 'warning'
 		);
 		$list = Array ();
@@ -235,17 +260,20 @@ static function send_CE($CE) {
 		$options['nexturl']=$nexturl;
 		$joptions=json_encode($options);
 		?>
-		<div id="vpl_root" class="vpl_ide vpl_ide_root ui-widget">
-		<div id="vpl_ide_dialog_progress" class="vpl_ide_dialog"
-			style="display: none;">
-			<div class="vpl_ide_progressbar">
-				<div class="vpl_ide_progressbarlabel"></div>
-			</div>
-		</div>
-		</div>
 		<script>
-			var evaluation = new VPL_Evaluation(<?php echo $joptions;?>);
+			VPL_Single_Evaluation(<?php echo $joptions;?>);
 	    </script>
+		<?php
+	}
+	static function generateBatchEvaluateScript($ajaxurls){
+		$options = Array();
+		$options['i18n']=self::i18n();
+		$options['ajaxurls']=$ajaxurls;
+		$joptions=json_encode($options);
+		?>
+		<script>
+			VPL_Batch_Evaluation(<?php echo $joptions;?>);
+		</script>
 		<?php
 	}		
 }
