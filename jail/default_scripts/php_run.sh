59c5df8bea784ed5a9d5b645d1c7e35e20b4376a
Juan Carlos Rodriguez-del-Pino <jcrodriguez@dis.ulpgc.es>
2015-01-28 13:16:55 +0000

    Added support to launch php server

--- jail/default_scripts/php_run.sh
+++ jail/default_scripts/php_run.sh
@@ -8,6 +8,29 @@
 #load common script and check programs
 . common_script.sh
 check_program php5
-cat common_script.sh > vpl_execution
-echo "php5 -n -f $VPL_SUBFILE0" >>vpl_execution
-chmod +x vpl_execution
+check_program x-www-browser
+if [ -f index.php ] ; then
+	SESSIONPATH=$HOME/.php_sessions
+	mkdir $SESSIONPATH
+	cp /etc/php5/cli/php.ini .php.ini
+	cat >> .php.ini <<FIN
+	
+session.save_path ="$SESSIONPATH"
+
+FIN
+    cat > vpl_wexecution <<"END_OF_SCRIPT"
+#!/bin/bash
+while true; do
+   	PHPPORT=$((6000+$RANDOM%25000))
+   	lsof -i :$PHPPORT
+   	[ "$?" != "0" ] && break
+done
+php -c .php.ini -d display_errors=On -S "127.0.0.1:$PHPPORT" &
+x-www-browser "127.0.0.1:$PHPPORT"
+END_OF_SCRIPT
+    chmod +x vpl_wexecution
+else
+    cat common_script.sh > vpl_execution
+    echo "php -n -f $VPL_SUBFILE0" >>vpl_execution
+    chmod +x vpl_execution
+fi
