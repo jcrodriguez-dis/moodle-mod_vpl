8adcc58693589eab096ac1a0698596e2ccad3851
Juan Carlos Rodriguez-del-Pino <jcrodriguez@dis.ulpgc.es>
2015-01-28 13:20:46 +0000

    Use gvhdl instead of ghdl

--- jail/default_scripts/vhdl_run.sh
+++ jail/default_scripts/vhdl_run.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-# $Id: vhdl_run.sh,v 1.3 2012-07-25 19:02:19 juanca Exp $
 # Default VHDL language run script for VPL
-# Copyright (C) 2012 Juan Carlos Rodríguez-del-Pino
+# Copyright (C) 2014 onward Juan Carlos Rodríguez-del-Pino
 # License http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
 # Author Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
 
 #load common script and check programs
 . common_script.sh
-check_program ghdl
+check_program gvhdl
 get_source_files vhdl vhd
 #compile
-ghdl -a $SOURCE_FILES
+gvhdl -a $SOURCE_FILES
 NAME=${VPL_SUBFILE0%.*}
-ghdl -e $NAME
+gvhdl -e $NAME
 if [ -f $NAME ] ; then
 	mv $NAME vpl_execution
 else
