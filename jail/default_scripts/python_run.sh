81f3497f2d1e1ea133ee1d619eb7456bac972e6a
Juan Carlos Rodriguez-del-Pino <jcrodriguez@dis.ulpgc.es>
2015-01-28 13:19:02 +0000

    Launch graphic interface when detected the use of Tkinter

--- jail/default_scripts/python_run.sh
+++ jail/default_scripts/python_run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# $Id: python_run.sh,v 1.4 2012-09-24 15:13:22 juanca Exp $
 # Default Python language run script for VPL
-# Copyright (C) 2012 Juan Carlos Rodríguez-del-Pino
+# Copyright (C) 2014 onward Juan Carlos Rodríguez-del-Pino
 # License http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
 # Author Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
 
@@ -11,3 +10,7 @@ check_program python
 cat common_script.sh > vpl_execution
 echo "python $VPL_SUBFILE0" >>vpl_execution
 chmod +x vpl_execution
+grep -E "^from[\t ]*Tkinter[\t ]*import" $VPL_SUBFILE0 2>&1 >/dev/null
+if [ "$?" -eq "0" ]	; then
+	mv vpl_execution vpl_wexecution
+fi
