c04a476c673459d45eb0e7b92ee9dea639d4206f
Juan Carlos Rodriguez-del-Pino <jcrodriguez@dis.ulpgc.es>
2015-02-04 16:34:36 +0000

    Base class for logging

---
+++ classes/event/base.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @package		VPL. Base class for log events
+ * @copyright	2014 onward Juan Carlos Rodríguez-del-Pino
+ * @license		http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author		Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
+ */
+
+namespace mod_vpl\event;
+require_once dirname(__FILE__).'/../../locallib.php';
+defined('MOODLE_INTERNAL') || die();
+
+abstract class base extends \core\event\base {
+	protected $legacy_action='';
+	protected function get_url_base($script){
+		$parms= array('id' => $this->contextinstanceid);
+		if(($this->relateduserid) && $this->relateduserid != $this->userid){
+			$parms['userid'] = $this->relateduserid;
+		}
+		return new \moodle_url('mod/vpl/'.$script,$parms);
+	}
+	public function get_description() {
+		return '';
+	}
+
+	public function get_url(){
+		return null;
+	}
+	public static function log($event_info){
+		$event = self::create($event_info);
+		$event->trigger();
+	}
+	public function get_legacy_logdata() {
+		$urltext='';
+		$url=$this->get_url();
+		if($url != null){
+			$urltext=$url->out(false);
+		}
+		return array(
+					$this->courseid,
+					VPL,
+					$this->legacy_action,
+				    $urltext,
+					$this->get_description(),
+					$this->contextinstanceid
+			);
+	}
+}
