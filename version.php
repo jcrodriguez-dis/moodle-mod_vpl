9f3fd3b194f987ea0cb6379ce5dd90d7e699c9d1
Juan Carlos Rodriguez-del-Pino <jcrodriguez@dis.ulpgc.es>
2015-02-05 17:55:24 +0000

    Update VPL version number

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015013116;	//Current module version 3.1.1
+$plugin->version = 2015020100;	//Current module version 3.1.1
 $plugin->cron    = 300; 		//cron check this module every 5 minutes
 $plugin->requires = 2014051200; // Moodle 2.7
 $plugin->maturity = MATURITY_STABLE;
