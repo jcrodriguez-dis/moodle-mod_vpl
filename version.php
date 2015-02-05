7197ac0fda41ede26da7ebfcb89c37f4a4547acb
Juan Carlos Rodriguez-del-Pino <jcrodriguez@dis.ulpgc.es>
2015-02-05 17:54:01 +0000

    Update requires to Moodle 2.7

--- version.php
+++ version.php
@@ -1,10 +1,10 @@
-<?php // $Id: version.php,v 1.22 2013-07-11 16:47:04 juanca Exp $
+<?php
 
 defined('MOODLE_INTERNAL') || die();
-//TODO Change module to plugin NOT changed for incompatibility with previous versions
-$plugin->version = 2014052912;	//Current module version 3.1
+
+$plugin->version = 2015013116;	//Current module version 3.1.1
 $plugin->cron    = 300; 		//cron check this module every 5 minutes
-$plugin->requires = 2013101800;
+$plugin->requires = 2014051200; // Moodle 2.7
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.1';
+$plugin->release = '3.1.1';
 $plugin->component = 'mod_vpl';
