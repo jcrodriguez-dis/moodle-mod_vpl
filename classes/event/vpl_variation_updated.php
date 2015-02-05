20f586d3c2a9ff6dcb93e994415288ebb3d559bc
Juan Carlos Rodriguez-del-Pino <jcrodriguez@dis.ulpgc.es>
2015-02-05 13:01:46 +0000

    New even for variation config update

--- classes/event/vpl_variation_updated.php
+++ classes/event/vpl_variation_updated.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @package		VPL. Class for logging of variation updated events
+ * @package		VPL. Class for logging of variation related events
  * @copyright	2014 onward Juan Carlos Rodríguez-del-Pino
  * @license		http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  * @author		Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
@@ -9,9 +9,12 @@ namespace mod_vpl\event;
 require_once dirname(__FILE__).'/../../locallib.php';
 defined('MOODLE_INTERNAL') || die();
 
-class vpl_variation_updated extends vpl_variation {
+class vpl_variation extends vpl {
 	protected function init() {
 		parent::init();
-		$this->data['crud'] = 'u';
+		$this->legacy_action='variations form';
 	}
+	public function get_description() {
+		return $this->get_description_mod('variation config');
+	}	
 }
