5d7b4d7addf6e8e58ea97c616b7122e588602ed5
Juan Carlos Rodriguez-del-Pino <jcrodriguez@dis.ulpgc.es>
2015-03-30 14:44:40 +0100

    Script improved

--- jail/default_scripts/scala_run.sh
+++ jail/default_scripts/scala_run.sh
@@ -4,9 +4,13 @@
 # Authors: Lang Michael: michael.lang.ima10@fh-joanneum.at
 #          Lückl Bernd: bernd.lueckl.ima10@fh-joanneum.at
 #          Lang Johannes: johannes.lang.ima10@fh-joanneum.at
+#          Peter Salhofer 2015
 #load common script and check programs
 . common_script.sh
 check_program scala
+check_program scalac
+APP=${VPL_SUBFILE0%.*}
+scalac $VPL_SUBFILE0
 cat common_script.sh > vpl_execution
-echo "scala -nocompdaemon $VPL_SUBFILE0" >> vpl_execution
+echo "scala -nocompdaemon -classpath ./ $APP" >> vpl_execution
 chmod +x vpl_execution
