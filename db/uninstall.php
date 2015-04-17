0c649ca6c550b1a000d9bd55f9d86f7b07029b57
Juan Carlos Rodriguez-del-Pino <jcrodriguez@dis.ulpgc.es>
2015-04-17 12:10:42 +0100

    Update licence comment and change tabs by spaces

--- db/uninstall.php
+++ db/uninstall.php
@@ -1,22 +1,35 @@
 <?php
+// This file is part of VPL for Moodle - http://vpl.dis.ulpgc.es/
+//
+// VPL for Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// VPL for Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with VPL for Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
  * Execute post-uninstall custom actions for VPL
- * 
- * @version		$Id: uninstall.php,v 1.1 2012-06-05 23:22:05 juanca Exp $
- * @package		VPL
- * @copyright	2012 Juan Carlos Rodríguez-del-Pino
- * @license		http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- * @author		Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
+ *
+ * @package mod_vpl
+ * @copyright 2012 Juan Carlos Rodríguez-del-Pino
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
  */
-
-defined('MOODLE_INTERNAL') || die();
-require_once $CFG->dirroot.'/mod/vpl/vpl.class.php';
+defined( 'MOODLE_INTERNAL' ) || die();
+require_once($CFG->dirroot . '/mod/vpl/vpl.class.php');
 $ret = true;
-$vpls = $DB->get_records('vpl',null,'','id');
+$vpls = $DB->get_records( 'vpl', null, '', 'id' );
 foreach ($vpls as $vplinstance) {
-	$vpl = new mod_vpl(null,$vplinstance->id);
-	$ret = $ret && $vpl->delete_all();
+    $vpl = new mod_vpl( null, $vplinstance->id );
+    $ret = $ret && $vpl->delete_all();
 }
-if(!$ret){
-	print_error('error deleting VPL');
+if (! $ret) {
+    print_error( 'error deleting VPL' );
 }
--- editor/editor_utility.php
+++ editor/editor_utility.php
@@ -1,15 +1,28 @@
 <?php
-/**
- * @version		$Id: editor_utility.php,v 1.8 2013-04-16 17:40:10 juanca Exp $
- * @package		VPL. editor utility functions
- * @copyright	2012 Juan Carlos Rodríguez-del-Pino
- * @license		http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- * @author		Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
- */
+// This file is part of VPL for Moodle - http://vpl.dis.ulpgc.es/
+//
+// VPL for Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// VPL for Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with VPL for Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Class with auxiliar functions for the editor
- **/
+ * IDE utility functions
+ *
+ * @package mod_vpl
+ * @copyright 2012 Juan Carlos Rodríguez-del-Pino
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
+ */
+
 
 class vpl_editor_util{
 	static function generate_jquery(){
