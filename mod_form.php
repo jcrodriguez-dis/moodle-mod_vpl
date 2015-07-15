8e968eda3c5b85ccdd61e4715950d56cacaaeb4b
Juan Carlos Rodriguez-del-Pino <jcrodriguez@dis.ulpgc.es>
2015-07-15 20:58:53 +0100

    Adapts the code to use add_intro_editor or standard_intro_elements based on Moodle version

--- mod_form.php
+++ mod_form.php
@@ -42,7 +42,11 @@ class mod_vpl_mod_form extends moodleform_mod {
         // shortdescription
         $mform->addElement('textarea', 'shortdescription', get_string('shortdescription',VPL), array('cols'=>70, 'rows'=>1));
         $mform->setType('shortdescription', PARAM_RAW);
-        $this->add_intro_editor(false,get_string('fulldescription',VPL));
+        if($CFG->version < 2015041700.00){ //Moodle version < 2.9Beta
+            $this->add_intro_editor(false,get_string('fulldescription',VPL)); //deprecated from 2.9beta
+        }else{
+            $this->standard_intro_elements(get_string('fulldescription',VPL));
+        }
         $mform->addElement('header', 'submissionperiod', get_string('submissionperiod', VPL));
         $secondsday=24*60*60;
         $now = time();
