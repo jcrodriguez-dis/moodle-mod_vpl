e03fd806777a757cd1e7d27ff00d727125f70c1f
Juan Carlos Rodriguez-del-Pino <jcrodriguez@dis.ulpgc.es>
2015-08-23 16:42:05 +0100

    Changes "gmcs", deprecated, by "mcs"

--- jail/default_scripts/csharp_debug.sh
+++ jail/default_scripts/csharp_debug.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# $Id: csharp_debug.sh,v 1.4 2012-09-24 15:13:22 juanca Exp $
-# Default C# language run script for VPL
+# Default C# language debug script for VPL
 # Copyright (C) 2012 Juan Carlos Rodríguez-del-Pino
 # License http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
 # Author Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
@@ -8,15 +7,14 @@
 #load common script and check programs
 . common_script.sh
 export MONO_ENV_OPTIONS=--gc=sgen
-export MONO_GC_PARAMS=max-heap-size=64m
-check_program gmcs
-check_program mdb
+check_program gdb
+check_program mcs
 #compile
-gmcs -out:output.exe *.cs
+mcs -debug -out:output.exe *.cs
 if [ -f output.exe ] ; then
+    echo "handle SIGXCPU SIG33 SIG35 SIGPWR nostop noprint" >> .dbinit
 	cat common_script.sh > vpl_execution
 	echo "export MONO_ENV_OPTIONS=--gc=sgen" >> vpl_execution
-	echo "export MONO_GC_PARAMS=max-heap-size=64m" >> vpl_execution
-	echo "mdb output.exe" >> vpl_execution
+	echo "gdb -args mono --debug output.exe" >> vpl_execution
 	chmod +x vpl_execution
 fi
--- jail/default_scripts/csharp_run.sh
+++ jail/default_scripts/csharp_run.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
-# $Id: csharp_run.sh,v 1.4 2012-09-24 15:13:21 juanca Exp $
 # Default C# language run script for VPL
-# Copyright (C) 2014 Juan Carlos Rodríguez-del-Pino
+# Copyright (C) Juan Carlos Rodríguez-del-Pino
 # License http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
 # Author Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
 
 #load common script and check programs
 . common_script.sh
-check_program gmcs
+check_program mcs
 check_program mono
 get_source_files cs
 #compile
 export MONO_ENV_OPTIONS=--gc=sgen
-export MONO_GC_PARAMS=max-heap-size=64m
-gmcs -pkg:dotnet -out:output.exe $SOURCE_FILES
+mcs -pkg:dotnet -out:output.exe $SOURCE_FILES
 if [ -f output.exe ] ; then
 	cat common_script.sh > vpl_execution
 	echo "export MONO_ENV_OPTIONS=--gc=sgen" >> vpl_execution
-	echo "export MONO_GC_PARAMS=max-heap-size=64m" >> vpl_execution
 	echo "mono output.exe" >> vpl_execution
 	chmod +x vpl_execution
 	for FILENAME in $SOURCE_FILES
