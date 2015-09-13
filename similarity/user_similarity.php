7bf02e404a4127fe9a8e2ec6a2876c935ab23232
Juan Carlos Rodriguez-del-Pino <jcrodriguez@dis.ulpgc.es>
2015-09-13 19:55:53 +0100

    Users similarity usable

--- similarity/user_similarity.php
+++ similarity/user_similarity.php
@@ -15,50 +15,74 @@
 // along with VPL for Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * @package mod_vpl. List most similar submission files
- * @copyright 2012 Juan Carlos Rodríguez-del-Pino
+ * List most similar submission files of one user in all activities
+ *
+ * @package mod_vpl
+ * @copyright Juan Carlos Rodríguez-del-Pino
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  * @author Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
  */
-die();
+
 require_once dirname(__FILE__).'/../../../config.php';
 require_once dirname(__FILE__).'/../locallib.php';
 require_once dirname(__FILE__).'/../vpl.class.php';
 require_once dirname(__FILE__).'/../vpl_submission.class.php';
 require_once dirname(__FILE__).'/similarity_factory.class.php';
 require_once dirname(__FILE__).'/similarity_base.class.php';
+require_once dirname(__FILE__).'/similarity_sources.class.php';
 require_once dirname(__FILE__).'/similarity_form.class.php';
 require_once dirname(__FILE__).'/clusters.class.php';
 require_once dirname(__FILE__).'/../views/status_box.class.php';
 ini_set('memory_limit','256M');
 
-$id = required_param('id', PARAM_INT);
+require_login();
+
+$id = required_param('id', PARAM_INT);   // course
 $userid = required_param('userid', PARAM_INT);
 $time_limit = 600; // 10 minutes
-$vpl = new mod_vpl($id);
-$vpl->prepare_page('similarity/user_similarity.php', array('id' => $id,'userid' => $userid));
+//Check course existence
+if (! $course = $DB->get_record("course", array('id' => $id))) {
+    print_error('invalidcourseid','',$id);
+}
+require_course_login($course);
+$user = $DB->get_record('user',array('id' => $userid));
+if(!$user){
+    print_error('invaliduserid','',$id);
+}
 
-$vpl->require_capability(VPL_SIMILARITY_CAPABILITY);
-\mod_vpl\event\vpl_user_similarity_report_viewed::log(array(
-        'objectid' => $vpl->get_instance()->id,
-        'context' => context_module::instance($id),
-        'relateduserid' => $userid
-));
-//Print header
+$strtitle=get_string('listsimilarity',VPL);
+$PAGE->set_url('/mod/vpl/similarity/user_similarity.php',array('id' => $id,'userid'=>$userid));
+$PAGE->navbar->add($strtitle);
 $PAGE->requires->css(new moodle_url('/mod/vpl/css/similarity.css'));
-$vpl->print_header(get_string('listsimilarity',VPL));
-$subinstance=$vpl->last_user_submission($userid);
-if($subinstance === false){ //No user submission
-    $vpl->print_footer();
-    die;
+$PAGE->set_title(fullname($user).':'.$strtitle);
+$PAGE->set_heading($course->fullname);
+echo $OUTPUT->header();
+echo $OUTPUT->heading(fullname($user));
+echo '<h2>'.$strtitle.'</h2>';
+//$vpl->require_capability(VPL_SIMILARITY_CAPABILITY);
+//\mod_vpl\event\vpl_similarity_report_viewed::log($vpl);
+//Print header
+
+$ovpls = get_all_instances_in_course(VPL,$course);
+$timenow = time();
+$vpls = array();
+//Get and select vpls to show
+foreach ($ovpls as $ovpl) {
+    $vpl = new mod_vpl(false,$ovpl->id);
+    $instance = $vpl->get_instance();
+    //Example NO
+    if($instance->example){
+        continue;
+    }
+    //Open and limited NO
+    if($timenow >= $instance->startdate && $timenow <= $instance->duedate){
+        continue;
+    }
+    $vpls[]=$vpl;
 }
-$submission = new mod_vpl_submission($vpl,$subinstance);
-//Get required filenames
-$filelist = $vpl->get_required_fgm()->getFileList();
-foreach($filelist as $filename){
-    $filesselected[$filename]=true;
-}
-//prepare table head
+
+@set_time_limit($time_limit);
+//Prepare table construction
 $firstname = get_string('firstname');
 $lastname  = get_string('lastname');
 if ($CFG->fullnamedisplay == 'lastname firstname') {
@@ -67,82 +91,52 @@ if ($CFG->fullnamedisplay == 'lastname firstname') {
     $name = $firstname.' / '.$lastname;
 }
 $with    = get_string('similarto',VPL);
-$head  = array ('#',$name, '',$with);
-$align = array ('right','left', 'center', 'left');
-$size = array ('','60','60','');
+$table = new html_table();
+$table->head  = array ($name, '',$with);
+$table->align = array ('Left', 'center', 'left');
+$table->size = array ('60','','60');
+$table->data = array();
 
-//Preprocess current VPL instance
-$subfgm = $submission->get_submitted_fgm();
-foreach($subfgm->getFileList() as $filename){
-    if(isset($filesselected[$filename])){ //Check if this is a required filename
-        echo '<h3>'.s($filename).'</h3>';
-        @set_time_limit($time_limit);
-        $simil = array();
-        $fselected = array();
-        $activity_load_box = new vpl_progress_bar(s($vpl->get_printable_name()).":".s($filename));
-        $fselected[$filename]=true;
-        vpl_similarity::scan_activity($simil,$vpl,$fselected,$activity_load_box);
-        //TODO Find and put first user file
-        $l=count($simil);
-        for($i=0; $i<$l;$i++){
-            if($simil[$i]->from->userid == $userid){
-                $obj= $simil[0];
-                $simil[0] = $simil[$i];
-                $simil[$i] = $obj;
-                break;
+$outputsize=array(1,1,1,2,2,2,2,3,3,3,3,3);
+//Process every activity selected
+
+$bars=array();
+foreach ($vpls as $vpl) {
+    //debugging("Adding activity files", DEBUG_DEVELOPER);
+    $simil =array();
+    vpl_similarity_preprocess::user_activity($simil,$vpl,$userid);
+    $nuserfiles=count($simil);
+    if($nuserfiles>0){
+        $activity_load_box = new vpl_progress_bar(s($vpl->get_printable_name()));
+        $bars[]=$activity_load_box;
+        vpl_similarity_preprocess::activity($simil,$vpl,array(),$activity_load_box);
+        $search_progression = new vpl_progress_bar(get_string('similarity',VPL));
+        $bars[]=$search_progression;
+        if($nuserfiles>= count($outputsize)){
+            $noutput=4;
+        }else{
+            $noutput=$outputsize[$nuserfiles];
+        }
+        $selected = vpl_similarity::get_selected($simil,$noutput,$nuserfiles,$search_progression);
+        if(count($selected)>0){
+            $table->data[] = array ($vpl->get_printable_name(),'','');
+            foreach($selected as $case){
+                $table->data[] = array (
+                        $case->first->show_info(),
+                        $case->get_link(),
+                        $case->second->show_info());
             }
         }
-        $search_progression = new vpl_progress_bar(get_string('similarity',VPL));
-        $selected = vpl_similarity::get_selected($simil,4,1,$search_progression);
-        $table = new html_table();
-        $table->head  = $head;
-        $table->align = $align;
-        $table->size = $size;
-        $usernumber=0;
-        foreach($selected as $case){
-            $table->data[] = array (++$usernumber,
-                                $case->first->show_info(),
-                                $case->get_link(),
-                                $case->second->show_info(),
-                                $case->get_cluster());
-        }
-        echo html_writer::table($table);
-        $activity_load_box->hide();
-        $search_progression->hide();
     }
 }
-@set_time_limit($time_limit);
-$simil = array();
-$activity_load_box = new vpl_progress_bar(s($vpl->get_printable_name()).":".s($filename));
-vpl_similarity::scan_activity($simil,$vpl,$filesselected,$activity_load_box,false);
-//TODO Find and put first user file
-$l=count($simil);
-$others=0;
-for($i=0; $i<$l;$i++){
-    if($simil[$i]->from->userid == $userid){
-        $obj= $simil[$others];
-        $simil[$others] = $simil[$i];
-        $simil[$i] = $obj;
-        $others++;
-    }
-}
-if($others>0){
-    $search_progression = new vpl_progress_bar(get_string('similarity',VPL));
-    $selected = vpl_similarity::get_selected($simil,2*$others,$others,$search_progression);
-    $table = new html_table();
-    $table->head  = $head;
-    $table->align = $align;
-    $table->size = $size;
-    $usernumber=0;
-    foreach($selected as $case){
-        $table->data[] = array (++$usernumber,
-                            $case->first->show_info(),
-                            $case->get_link(),
-                            $case->second->show_info());
-    }
-    echo html_writer::table($table);
-    $search_progression->hide();
-}
-$activity_load_box->hide();
 
-$vpl->print_footer();
+$extinfo=false;
+foreach($bars as $bar){
+    $bar->hide();
+}
+if(count($table->data)){
+    echo html_writer::table($table);
+}else{
+    echo $OUTPUT->box(get_string('noresults'));
+}
+echo $OUTPUT->footer();
