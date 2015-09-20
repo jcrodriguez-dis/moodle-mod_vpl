0c8c2c50a72569f7e9a598c403e079a5a6b3dbeb
Juan Carlos Rodriguez-del-Pino <jcrodriguez@dis.ulpgc.es>
2015-09-20 07:58:51 +0100

    Clean up

--- similarity/similarity_base.class.php
+++ similarity/similarity_base.class.php
@@ -24,183 +24,8 @@
  */
 
 require_once dirname(__FILE__).'/tokenizer_factory.class.php';
+require_once dirname(__FILE__).'/similarity_sources.class.php';
 require_once dirname(__FILE__).'/../views/status_box.class.php';
-
-/**
- * @abstract class to represent files from any source
- */
-class vpl_file_from_base{
-    public function show_info(){
-    }
-    public function can_access(){
-        return false;
-    }
-    public function get_userid(){
-        return '';
-    }
-}
-
-/**
- * Information of a file from a directory
- */
-class vpl_file_from_dir extends vpl_file_from_base{
-    static $usersname=array();
-    var $dirname;
-    var $filename;
-    var $userid;
-    //This is for compatibility with GAP 2.x application
-    static public function process_gap_userfile($filepath){
-        if(strtolower(basename($filepath)) == 'datospersonales.gap'){
-            $NIF = '';
-            $nombre='';
-            $apellidos='';
-            $lines = explode("\n",file_get_contents($filepath));
-            if(count($lines)>3){
-                if(strpos($lines[1],'NIF=')!==false){
-                    $NIF = substr($lines[1],4);
-                }
-                if(strpos($lines[2],'Nombre=')!==false){
-                    $nombre = substr($lines[2],7);
-                }
-                if(strpos($lines[3],'Apellidos=')!==false){
-                    $apellidos = substr($lines[3],10);
-                }
-            }
-            if($NIF>'' && $nombre>'' && $apellidos>''){
-                global $CFG;
-                if ($CFG->fullnamedisplay == 'lastname firstname') {
-                    self::$usersname[$NIF]=mb_convert_encoding($apellidos.', '.$nombre,'utf-8');
-                } else {
-                    self::$usersname[$NIF]=mb_convert_encoding($nombre.' '.$apellidos,'utf-8');
-                }
-            }
-        }
-    }
-
-    //This is for compatibility with GAP 2.x application
-    static public function get_user_id_from_file($filename){
-        if(count(self::$usersname)){
-            $filename = strtolower($filename);
-            foreach(self::$usersname as $userid => $userdata){
-                if(strpos($filename,$userid) !== false){
-                    return $userid;
-                }
-            }
-        }
-        return '';
-    }
-
-    function __construct(&$filename,$dirname,$userid=''){
-        $this->filename=$filename;
-        $this->dirname=$dirname;
-        $this->userid=self::get_user_id_from_file($filename);
-    }
-
-    function get_userid(){
-        return $this->userid;
-    }
-    public function show_info(){
-        $ret='';
-        $ret .='<a href="'.'file.php'.'">';
-        $ret .=s($this->filename).' ';
-        $ret .= '</a>';
-        if($this->userid!=''){
-            $ret .= ' '.self::$usersname[$this->userid];
-        }
-        return $ret;
-    }
-    public function can_access(){
-        return true;
-    }
-    public function link_parms($t){
-        $res = array('type'.$t=>2,'dirname'.$t => $this->dirname,'filename'.$t => $this->filename);
-        if($this->userid!=''){
-            $res['username'.$t]=self::$usersname[$this->userid];;
-        }
-        return $res;
-    }
-}
-
-/**
- * Information of a file from a zip file
- *
- */
-class vpl_file_from_zipfile extends vpl_file_from_dir{
-    function __construct(&$filename,$zipname,$userid=''){
-        parent::__construct($filename,$zipname,$userid);
-    }
-    public function show_info(){
-        $ret='';
-        $ret .=s($this->filename);
-        if($this->userid!=''){
-            $ret .= ' '.self::$usersname[$this->userid];
-        }
-        return $ret;
-    }
-    public function can_access(){
-        return true;
-    }
-    public function link_parms($t){
-        $res = array('type'.$t=>3,'zipfile'.$t=>$this->dirname,'filename'.$t => $this->filename);
-        if($this->userid!=''){
-            $res['username'.$t]=self::$usersname[$this->userid];;
-        }
-        return $res;
-    }
-}
-
-/**
- * Information of a file from other vpl activity
- *
- */
-class vpl_file_from_activity extends vpl_file_from_base{
-    static $vpls=array();
-    var $vplid;
-    var $filename;
-    var $subid;
-    var $userid;
-    function __construct(&$filename,&$vpl,$subinstance){
-        $id = $vpl->get_instance()->id;
-        if(!isset(self::$vpls[$id])){
-            self::$vpls[$id]=$vpl;
-        }
-        $this->vplid = $id;
-        $this->filename=$filename;
-        $this->userid=$subinstance->userid;
-        $this->subid=$subinstance->id;
-    }
-    public function show_info(){
-        global $DB;
-        $vpl = self::$vpls[$this->vplid];
-        $cmid = $vpl->get_course_module()->id;
-        $ret='';
-        if($this->userid>=0){
-            $user = $DB->get_record('user',array('id' => $this->userid));
-        }else{
-            $user = false;
-        }
-        if($user){
-            $ret .='<a href="'.vpl_mod_href('/forms/submissionview.php','id',$cmid,'userid',$user->id).'">';
-        }
-        $ret .=s($this->filename);
-        if($user){
-            $ret .= '</a> ';
-            $sub = new mod_vpl_submission($vpl,$this->subid);
-            $ret .=    $sub->print_grade_core().'<br />';
-            $ret .= $vpl->user_fullname_picture($user);
-        }
-        return $ret;
-    }
-    function get_userid(){
-        return $this->userid;
-    }
-    public function can_access(){
-        return true;
-    }
-    public function link_parms($t){
-        return array('type'.$t=>1,'subid'.$t=>$this->subid,'filename'.$t => $this->filename);
-    }
-}
 
 
 /**
@@ -211,6 +36,7 @@ class vpl_file_from_activity extends vpl_file_from_base{
 class vpl_similarity_base{
     var $from;
     var $size;
+    var $sizeh;
     static $value_converter=array(); //array to convert string operators to numbers
     var $vecfrec;
     var $hashses;
@@ -229,11 +55,12 @@ class vpl_similarity_base{
     public function sintax_normalize(&$tokens){
         return $tokens;
     }
-    public function init(&$data,&$from){
+    public function init(&$data,$from, $toRemove=null){
         $HASH_SIZE = 4;
         $HASH_REDUCTION = 1000;
         $this->from=$from;
         $this->size=0;
+        $this->sizeh=0;
         $this->vecfrec = array();
         $this->hashes = array();
         //Get tokenizer for this language
@@ -266,7 +93,7 @@ class vpl_similarity_base{
                 }else{
                     $this->hashes[$hash] = 1;
                 }
-                $this->size++;
+                $this->sizeh++;
                 //Get operator id
                 $vid = self::get_value_id($token->value);
                 if(isset($this->vecfrec[$vid])){
@@ -274,7 +101,30 @@ class vpl_similarity_base{
                 }else{
                     $this->vecfrec[$vid]=1;
                 }
+                $this->size++;
             }
+        }
+        if($toRemove != null){
+            foreach($toRemove->vecfrec as $id => $frec){
+                if(isset($this->vecfrec[$id])){
+                    $this->vecfrec[$id] = $this->vecfrec[$id]>$frec?$this->vecfrec[$id]-$frec:0;
+                }
+            }
+            foreach($toRemove->hashes as $id => $frec){
+                if(isset($this->hashes[$id])){
+                    $this->hashes[$id] = $this->hashes[$id]>$frec?$this->hashes[$id]-$frec:0;
+                }
+            }
+            $newsize=0;
+            foreach($this->vecfrec as $frec){
+                $newsize += $frec;
+            }
+            $this->size=$newsize;
+            $newsize=0;
+            foreach($this->hashes as $frec){
+                $newsize += $frec;
+            }
+            $this->sizeh=$newsize;
         }
     }
     public function show_info($ext=false){
@@ -355,8 +205,8 @@ class vpl_similarity_base{
                 $dif+=$frec;
             }
         }
-        $dif += $other->size - $taken;
-        return 100*(1-($dif/($this->size+$other->size)));
+        $dif += $other->sizeh - $taken;
+        return 100*(1-($dif/($this->sizeh+$other->sizeh)));
     }
 }
 
@@ -422,7 +272,7 @@ class vpl_files_pair{
         $text .= (int)$this->s3;
         $text .= '</spam>';
         if($this->first->can_access() && $this->second->can_access()){
-            $url = vpl_mod_href('similarity/diff.php','id',required_param('id', PARAM_INT));
+            $url = vpl_mod_href('similarity/diff.php');
             foreach($this->first->link_parms('1') as $parm => $value){
                 $url = vpl_url_add_param($url,$parm,$value);
             }
@@ -493,172 +343,6 @@ class vpl_files_pair{
  *
  */
 class vpl_similarity{
-    /**
-     * scan_activity load activity files into $simil array
-     * @param $simil array of file processed objects
-     * @param $vpl activity to process
-     * @param $filesselected array if set only files selected or unselected will be processed
-     * @param $SPB Box used to show load process
-     * @param $selected boolean switch to get selected or no selected files (default true)
-     * @return void
-     */
-    static public function scan_activity(&$simil, $vpl, $filesselected=array(), $SPB, $selected=true){
-        global $CFG;
-        $vpl->require_capability(VPL_SIMILARITY_CAPABILITY);
-        $list = $vpl->get_students();
-        if(count($list) == 0){
-            return;
-        }
-        $submissions = $vpl->all_last_user_submission();
-        $SPB->set_max(count($list));
-        //debugging("Submissions ".count($submissions), DEBUG_DEVELOPER);
-        $i = 0;
-        foreach ($list as $user) {
-            $i++;
-            $SPB->set_value($i);
-            if(isset($submissions[$user->id])){
-                $subinstance = $submissions[$user->id];
-                $origin = '';
-                $submission = new mod_vpl_submission($vpl,$subinstance);
-                $subf = $submission->get_submitted_fgm();
-                $filelist = $subf->getFileList();
-                foreach($filelist as $filename){
-                    //debugging("Filename ".$filename, DEBUG_DEVELOPER);
-                    if($selected){
-                        if(count($filesselected)>0 && !isset($filesselected[$filename])){
-                            continue;
-                        }
-                    }else{
-                        if(isset($filesselected[$filename])){
-                            continue;
-                        }
-                    }
-                    //debugging("Added ".$filename, DEBUG_DEVELOPER);
-                    $sim = vpl_similarity_factory::get($filename);
-                    if($sim){
-                        $data = $subf->getFileData($filename);
-                        $from =new vpl_file_from_activity($filename,$vpl,$subinstance);
-                        $sim->init($data,$from);
-                        if($sim->size>10){
-                            $simil[]=$sim;
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    static public function get_zip_filepath($zipname){
-        global $CFG,$COURSE;
-        $zipname=basename($zipname);
-        return $CFG->dataroot . '/temp/vpl_zip/'.$COURSE->id.'_'.$zipname;
-    }
-
-    static public function create_zip_file($zipname, $zipdata){
-        $filename = self::get_zip_filepath($zipname);
-        $fp = vpl_fopen($filename);
-        fwrite($fp,$zipdata);
-        fclose($fp);
-    }
-
-    static public function scan_zip(&$simil, $zipname, $zipdata, $vpl, $filesselected=array(), $SPB){
-        global $CFG;
-        $ext = strtoupper(pathinfo($zipname,PATHINFO_EXTENSION));
-        if($ext != 'ZIP'){
-            print_error('nozipfile');
-        }
-        self::create_zip_file($zipname, $zipdata);
-        $zip = new ZipArchive();
-        $zipfilename=self::get_zip_filepath($zipname);
-        //debugging("Unzip file ".$zipfilename, DEBUG_DEVELOPER);
-        $SPB->set_value(get_string('unzipping',VPL));
-        if($zip->open($zipfilename)){
-            $SPB->set_max($zip->numFiles);
-            $i=1;
-            for($i=0; $i < $zip->numFiles ;$i++){
-                $SPB->set_value($i+1);
-                $filename = $zip->getNameIndex($i);
-                if($filename==false) break;
-                $data = $zip->getFromIndex($i);
-                if($data){
-                    //debugging("Examining file ".$filename, DEBUG_DEVELOPER);
-                    //TODO remove if no GAP file
-                    vpl_file_from_zipfile::process_gap_userfile($filename);
-                    if(count($filesselected)>0 && !isset($filesselected[basename($filename)])){
-                        continue;
-                    }
-                    $sim = vpl_similarity_factory::get($filename);
-                    if($sim){
-                        //TODO locate userid
-                        $from =new vpl_file_from_zipfile($filename,$zipname);
-                        $sim->init($data,$from);
-                        if($sim->size>10){
-                            $simil[]=$sim;
-                        }
-                    }
-                }
-            }
-        }
-        $SPB->set_value($zip->numFiles);
-    }
-    static function get_dir_filelist($basepath,$dir){
-        $filelist = array();
-        $dirlist = array();
-        if($dir > ''){
-            $basedir = $basepath.'/'.$dir;
-        }else{
-            $basedir = $basepath;
-        }
-        if($dd = opendir($basedir)){
-            while ($file=readdir($dd)) {
-                if ($file[0] == '.'){
-                    continue;
-                }
-                $relpath =$dir.'/'.$file;
-                $fullpath=$basedir.'/'.$file;
-                if(is_file($fullpath)){
-                    $filelist[] = $relpath;
-                }elseif(is_dir($fullpath)){
-                    $dirlist[] = $relpath;
-                }
-            }
-            closedir($dd);
-        }
-        foreach($dirlist as $adir){
-            $otherlist = self::get_dir_filelist($basepath,$adir);
-            $filelist = array_merge($filelist,$otherlist);
-        }
-        return $filelist;
-    }
-    static public function scan_directory(&$simil, $dir, $vpl, $filesselected=array(), $SPB){
-        global $CFG;
-        $basedir = $CFG->dataroot.'/'.$vpl->get_course()->id;
-        $SPB->set_value(get_string('scanningdir',VPL));
-        $filelist = self::get_dir_filelist($basedir.'/'.$dir,'');
-        $SPB->set_value((string)count($filelist));
-        $SPB->set_max(count($filelist));
-        foreach($filelist as $pos => $file){
-            $SPB->set_value($pos+1);
-            $base = basename($file);
-            //TODO remove if no GAP file
-            $fullpath = $basedir.'/'.$dir.'/'.$file;
-            vpl_file_from_dir::process_gap_userfile($fullpath);
-            if(count($filesselected)>0 && !isset($filesselected[$base])){
-                continue;
-            }
-            $sim = vpl_similarity_factory::get($file);
-            if($sim){
-                $data = file_get_contents($fullpath);
-                //TODO locate userid
-                $from =new vpl_file_from_dir($file,$dir);
-                $sim->init($data,$from);
-                if($sim->size>10){
-                    $simil[]=$sim;
-                }
-            }
-        }
-    }
-
     static public function get_selected(&$files, $maxselected, $slimit, $SPB){
         $vs1=array();
         $vs2=array();
