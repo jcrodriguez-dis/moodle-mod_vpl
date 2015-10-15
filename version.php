763b700fc1b7c806b0f4b3997931a741c7cc055e
Juan Carlos Rodriguez-del-Pino <jcrodriguez@dis.ulpgc.es>
2015-10-15 17:23:58 +0100

    Line delimiters are converted to Unix format

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 
 
 defined('MOODLE_INTERNAL') || die();
-$plugin->version = 2015021000;
+$plugin->version = 2015101300;
 $plugin->cron    = 300; 		//cron check this plugin every 5 minutes
 $plugin->requires = 2014051200; // Moodle 2.7
 $plugin->maturity = MATURITY_STABLE;
