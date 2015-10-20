a5055bed20a36c8261c49f5d86463cfdb8da492d
Juan Carlos Rodriguez-del-Pino <jcrodriguez@dis.ulpgc.es>
2015-10-20 13:43:58 +0100

    Uses Moodle code style

--- similarity/similarity_matlab.class.php
+++ similarity/similarity_matlab.class.php
@@ -1,56 +1,63 @@
 <?php
+// This file is part of VPL for Moodle - http://vpl.dis.ulpgc.es/
+//
+// VPL for Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// VPL for Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with VPL for Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
- * @version		$Id: similarity_matlab.class.php,v 1.2 2013-06-11 18:28:29 juanca Exp $
- * @package		VPL. C language similarity class
- * @copyright	2012 Juan Carlos Rodríguez-del-Pino
- * @license		http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- * @author		Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
+ * M (Octave) language similarity class
+ *
+ * @package mod_vpl
+ * @copyright 2012 Juan Carlos Rodríguez-del-Pino
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
  */
-require_once dirname(__FILE__).'/similarity_base.class.php';
+require_once(dirname(__FILE__).'/similarity_base.class.php');
 
-class vpl_similarity_matlab extends vpl_similarity_base{
-	public function get_type(){
-		return 9;
-	}
-
-	public function sintax_normalize(&$tokens){
-		$pos_ini_inst = 0;
-		$open_brace =false;
-		$nsemicolon=0;
-		$ret = array();
-		$prev = new vpl_token(vpl_token_type::identifier,'',0);
-		foreach($tokens as $token){
-			if($token->type == vpl_token_type::operator){
-				switch($token->value){
-					case '[':
-						//only add ]
-						break;
-					case '(':
-						//only add )
-						break;
-					case '{':
-						break;
-					case '<': //Replace < by >.
-						$token->value='>';
-						$ret[]=$token;
-						break;
-					case '<=': //Replace < by >.
-						$token->value='>=';
-						$ret[]=$token;
-						break;
-					default:
-						$ret[]=$token;
-				}
-				$prev=$token;
-			}
-			//TODO remove (p)
-		}
-		return $ret;
-	}
-
-	public function get_tokenizer(){
-		return vpl_tokenizer_factory::get('matlab');
-	}
+class vpl_similarity_matlab extends vpl_similarity_base {
+    public function get_type() {
+        return 9;
+    }
+    public function sintax_normalize(&$tokens) {
+        $ret = array ();
+        foreach ($tokens as $token) {
+            if ($token->type == vpl_token_type::operator) {
+                switch ($token->value) {
+                    case '[' :
+                        // Only add ].
+                        break;
+                    case '(' :
+                        // Only add ).
+                        break;
+                    case '{' :
+                        break;
+                    case '<' : // Replace < by >.
+                        $token->value = '>';
+                        $ret [] = $token;
+                        break;
+                    case '<=' : // Replace < by >.
+                        $token->value = '>=';
+                        $ret [] = $token;
+                        break;
+                    default :
+                        $ret [] = $token;
+                }
+            }
+            // TODO remove "(p)" .
+        }
+        return $ret;
+    }
+    public function get_tokenizer() {
+        return vpl_tokenizer_factory::get( 'matlab' );
+    }
 }
-
-?>
--- similarity/similarity_python.class.php
+++ similarity/similarity_python.class.php
@@ -1,111 +1,121 @@
 <?php
+// This file is part of VPL for Moodle - http://vpl.dis.ulpgc.es/
+//
+// VPL for Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// VPL for Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with VPL for Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
- * @version		$Id: similarity_python.class.php,v 1.2 2013-06-11 18:31:10 juanca Exp $
- * @package		VPL. Python language similarity class
- * @copyright	2012 Juan Carlos Rodríguez-del-Pino
- * @license		http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- * @author		Lang Michael <michael.lang.ima10@fh-joanneum.at>
- * @author		Lückl Bernd <bernd.lueckl.ima10@fh-joanneum.at>
- * @author		Lang Johannes <johannes.lang.ima10@fh-joanneum.at>
- * @author		Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
+ * Python language similarity class
+ *
+ * @package mod_vpl
+ * @copyright 2012 Juan Carlos Rodríguez-del-Pino
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author Lang Michael <michael.lang.ima10@fh-joanneum.at>
+ * @author Lückl Bernd <bernd.lueckl.ima10@fh-joanneum.at>
+ * @author Lang Johannes <johannes.lang.ima10@fh-joanneum.at>
+ * @author Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
  */
-require_once dirname(__FILE__).'/similarity_base.class.php';
 
-class vpl_similarity_python extends vpl_similarity_base{
-	public function get_type(){
-		return 8;
-	}
-	public function sintax_normalize(&$tokens){
-		$open_brace =false;
-		$nsemicolon=0;
-		$ret = array();
-		$prev = new vpl_token(vpl_token_type::identifier,'',0);
-		foreach($tokens as $token){
-			if($token->type == vpl_token_type::operator){
-				//++ and -- operator
-				//:: operator
-				//(*p). and p->
-				//+=, -=, *=, etc
-				switch($token->value){
-					case '[':
-						//only add ]
-						break;
-					case '(':
-						//only add )
-						break;
-					case '{':
-						//only add }
-						$nsemicolon=0;
-						$open_brace =true;
-						break;
-					case '}':
-						//Remove unneeded {}
-						if(!($open_brace && $nsemicolon<2)){
-							$ret[]=$token;
-						}
-						$open_brace =false;
-						break;
-					case ';':
-						//count semicolon after a {
-						$nsemicolon++;
-						$ret[]=$token;
-						break;
-					case '+=':
-						$token->value='=';
-						$ret[]=$token;
-						$token->value='+';
-						$ret[]=$token;
-						break;
-					case '-=':
-						$token->value='=';
-						$ret[]=$token;
-						$token->value='-';
-						$ret[]=$token;
-						break;
-					case '*=':
-						$token->value='=';
-						$ret[]=$token;
-						$token->value='*';
-						$ret[]=$token;
-						break;
-					case '/=':
-						$token->value='=';
-						$ret[]=$token;
-						$token->value='/';
-						$ret[]=$token;
-						break;
-					case '%=':
-						$token->value='=';
-						$ret[]=$token;
-						$token->value='%';
-						$ret[]=$token;
-						break;
-					case '->':
-						if($prev->value == 'this'){
-							break;
-						}
-						$token->value='(';
-						$ret[]=$token;
-						$token->value='*';
-						$ret[]=$token;
-						$token->value=')';
-						$ret[]=$token;
-						$token->value='.';
-						$ret[]=$token;
-						break;
-					default:
-						$ret[]=$token;
-				}
-				$prev=$token;
-			}
-			//TODO remove (p)
-		}
-		return $ret;
-	}
-	
-	public function get_tokenizer(){
-		return vpl_tokenizer_factory::get('python');
-	}
+require_once(dirname(__FILE__).'/similarity_base.class.php');
+
+class vpl_similarity_python extends vpl_similarity_base {
+    public function get_type() {
+        return 8;
+    }
+    public function sintax_normalize(&$tokens) {
+        $openbrace = false;
+        $nsemicolon = 0;
+        $ret = array ();
+        $prev = new vpl_token( vpl_token_type::identifier, '', 0 );
+        foreach ($tokens as $token) {
+            if ($token->type == vpl_token_type::operator) {
+                switch ($token->value) {
+                    case '[' :
+                        // Only add ].
+                        break;
+                    case '(' :
+                        // Only add ).
+                        break;
+                    case '{' :
+                        // Only add }.
+                        $nsemicolon = 0;
+                        $openbrace = true;
+                        break;
+                    case '}' :
+                        // Remove unneeded {}.
+                        if (! ($openbrace && $nsemicolon < 2)) {
+                            $ret [] = $token;
+                        }
+                        $openbrace = false;
+                        break;
+                    case ';' :
+                        // Count semicolon after a {.
+                        $nsemicolon ++;
+                        $ret [] = $token;
+                        break;
+                    case '+=' :
+                        $token->value = '=';
+                        $ret [] = $token;
+                        $token->value = '+';
+                        $ret [] = $token;
+                        break;
+                    case '-=' :
+                        $token->value = '=';
+                        $ret [] = $token;
+                        $token->value = '-';
+                        $ret [] = $token;
+                        break;
+                    case '*=' :
+                        $token->value = '=';
+                        $ret [] = $token;
+                        $token->value = '*';
+                        $ret [] = $token;
+                        break;
+                    case '/=' :
+                        $token->value = '=';
+                        $ret [] = $token;
+                        $token->value = '/';
+                        $ret [] = $token;
+                        break;
+                    case '%=' :
+                        $token->value = '=';
+                        $ret [] = $token;
+                        $token->value = '%';
+                        $ret [] = $token;
+                        break;
+                    case '->' :
+                        if ($prev->value == 'this') {
+                            break;
+                        }
+                        $token->value = '(';
+                        $ret [] = $token;
+                        $token->value = '*';
+                        $ret [] = $token;
+                        $token->value = ')';
+                        $ret [] = $token;
+                        $token->value = '.';
+                        $ret [] = $token;
+                        break;
+                    default :
+                        $ret [] = $token;
+                }
+                $prev = $token;
+            }
+            // TODO remove (p).
+        }
+        return $ret;
+    }
+    public function get_tokenizer() {
+        return vpl_tokenizer_factory::get( 'python' );
+    }
 }
-
-?>
