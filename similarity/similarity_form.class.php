33d5d5fabb6cc79b705593f741cb00f223182b5f
Juan Carlos Rodriguez-del-Pino <jcrodriguez@dis.ulpgc.es>
2015-10-20 13:16:51 +0100

    Uses Moodle code style

--- similarity/similarity_form.class.php
+++ similarity/similarity_form.class.php
@@ -1,133 +1,138 @@
 <?php
+// This file is part of VPL for Moodle - http://vpl.dis.ulpgc.es/
+//
+// VPL for Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// VPL for Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with VPL for Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
- * @version		$Id: similarity_form.class.php,v 1.15 2013-06-10 08:30:21 juanca Exp $
- * @package		VPL. Similarity form class
- * @copyright	2012 Juan Carlos Rodríguez-del-Pino
- * @license		http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- * @author		Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
+ * Similarity form class
+ *
+ * @package mod_vpl
+ * @copyright 2012 Juan Carlos Rodríguez-del-Pino
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
  */
 
 global $CFG;
-require_once dirname(__FILE__).'/../../../config.php';
-require_once $CFG->libdir.'/formslib.php';
-class vpl_similarity_form extends moodleform {
-	private $vpl;
-	
-	function __construct($page,$vpl){
-		$this->vpl = $vpl;
-		parent::__construct($page);
-	}
-	function list_activities($vplid){
-		global $DB;
-		$list=array(''=>'');
-		$cn = $this->vpl->get_course()->shortname;
-		//Low privilegies
-		$courses=get_user_capability_course(VPL_VIEW_CAPABILITY,null,true,'shortname');
-		//Reorder courses by name similar to current
-		usort($courses, function ($a, $b) use ($cn){
-			$na = $a->shortname;
-			$nb = $b->shortname;
-			$da= levenshtein($na,$cn);
-			$db= levenshtein($nb,$cn);
-			if($da != $db) return ($da < $db)?-1:1;
-			if($na == $cn) return -1;
-			if($nb == $cn) return 1;
-			if($na != $nb) return ($na < $nb)?-1:1;
-			return 0;
-		});
-		foreach($courses as $course){
-			$vpls = $DB->get_records(VPL,array('course' => $course->id));
-			foreach($vpls as $vplinstace){
-				if($vplinstace->id == $vplid){
-					continue;
-				}
-				$othervpl = new mod_vpl(false,$vplinstace->id);
-				if(! $othervpl->get_course_module()){
-					continue;
-				}
-				if($othervpl->has_capability(VPL_SIMILARITY_CAPABILITY)){
-					$list[$othervpl->get_course_module()->id]=$othervpl->get_course()->shortname.' '.$othervpl->get_printable_name();
-				}
-			}
-			if(count($list)>1000){
-				break; //Stop loading instances
-			}
-		}
-		$list['']=get_string('select');
-		return $list;
-	}
-	
-	function get_directories($dirpath){
-        $ret = array();
-        $dd = @opendir($dirpath);
-        if($dd !== false){
-	        while ($dir=readdir($dd)) {
-	            if ($dir!='.' && $dir!='..' && is_dir($dirpath."/".$dir)) {
-	                $ret[] = $dir;
-	            }
-	        }
-        	closedir($dd);
-        }
-        return $ret;
-    }
 
-	function list_directories($cid){
-		global $CFG;
-		$dirs=array();
-		$dirs=array('' => get_string('select'));
-		$basedir=$CFG->dataroot.'/'.$cid;
-		foreach($this->get_directories($basedir) as $dir){
-			$dirs[$dir]=$dir;
-			foreach($this->get_directories($basedir.'/'.$dir) as $inner){
-				$dirs[$dir.'/'.$inner]=$dir.'/'.$inner;
-			}
-		}
-		return $dirs;
-	}
-	function definition(){
-		$mform    =& $this->_form; 
-        $mform->addElement('hidden','id',required_param('id',PARAM_INT));
-		$mform->setType('id', PARAM_INT);
+require_once(dirname(__FILE__).'/../../../config.php');
+require_once($CFG->libdir.'/formslib.php');
+
+class vpl_similarity_form extends moodleform {
+    private $vpl;
+    public function __construct($page, $vpl) {
+        $this->vpl = $vpl;
+        parent::__construct( $page );
+    }
+    protected function list_activities($vplid) {
+        global $DB;
+        global $USER;
+        $list = array (
+                '' => ''
+        );
+        $cn = $this->vpl->get_course()->shortname;
+        // Get all courses the user is enroled.
+        $courses = enrol_get_all_users_courses( $USER->id );
+        // Reorder courses by name similar to current.
+        usort( $courses, function ($a, $b) use($cn) {
+            $na = $a->shortname;
+            $nb = $b->shortname;
+            $da = levenshtein( $na, $cn );
+            $db = levenshtein( $nb, $cn );
+            if ($da != $db) {
+                return ($da < $db) ? - 1 : 1;
+            }
+            if ($na == $cn) {
+                return - 1;
+            }
+            if ($nb == $cn) {
+                return 1;
+            }
+            if ($na != $nb) {
+                return ($na < $nb) ? - 1 : 1;
+            }
+            return 0;
+        } );
+        foreach ($courses as $course) {
+            // TODO add index in VPL table to accelerate this query.
+            $vpls = $DB->get_records( VPL, array (
+                    'course' => $course->id
+            ) );
+            foreach ($vpls as $vplinstace) {
+                if ($vplinstace->id == $vplid) {
+                    continue;
+                }
+                $othervpl = new mod_vpl( false, $vplinstace->id );
+                if (! $othervpl->get_course_module()) {
+                    continue;
+                }
+                if ($othervpl->has_capability( VPL_SIMILARITY_CAPABILITY )) {
+                    $list [$othervpl->get_course_module()->id] = $othervpl->get_course()->shortname
+                                                                 . ' ' . $othervpl->get_printable_name();
+                }
+            }
+            if (count( $list ) > 1000) {
+                break; // Stop loading instances.
+            }
+        }
+        $list [''] = get_string( 'select' );
+        return $list;
+    }
+    protected function definition() {
+        $mform = & $this->_form;
+        $mform->addElement( 'hidden', 'id', required_param( 'id', PARAM_INT ) );
+        $mform->setType( 'id', PARAM_INT );
+        $mform->addElement( 'header', 'scanoptions', get_string( 'scanoptions', VPL ) );
+        $defaultlimit = (intval( count( $this->vpl->get_submissions_number() ) / 25 ) + 1) * 5;
+        $options = array ();
+        $options [$defaultlimit] = $defaultlimit;
+        for ($i = 5; $i <= 40; $i += 5) {
+            $options [$i] = $i;
+        }
+        for ($i = 60; $i <= 100; $i += 20) {
+            $options [$i] = $i;
+        }
+        for ($i = 150; $i <= 400; $i += 50) {
+            $options [$i] = $i;
+        }
+        asort( $options );
+        $mform->addElement( 'select', 'maxoutput', get_string( 'maxsimilarityoutput', VPL ), $options );
+        $mform->setType( 'maxoutput', PARAM_INT );
+        $mform->setDefault( 'maxoutput', $defaultlimit );
         $filelist = $this->vpl->get_required_fgm()->getFileList();
-		if(count($filelist)>0){
-	        $mform->addElement('header', 'headerfilestoprocess', get_string('filestoscan', VPL));
-			$num=0;
-			foreach($filelist as $filename){
-				$mform->addElement('checkbox','file'.$num,$filename);
-				$mform->setDefault('file'.$num, true);
-				$mform->disabledIf('file'.$num, 'allfiles','checked');
-				$num++;
-			}
-			$mform->addElement('checkbox','allfiles',get_string('allfiles',VPL));
-			$mform->addElement('checkbox','joinedfiles',get_string('joinedfiles',VPL));
-			$mform->disabledIf('joinedfiles', 'allfiles','checked');
-		}else{
-			$mform->addElement('hidden','allfiles','checked');
-			$mform->setType('allfiles', PARAM_BOOL);
-		}
-		$mform->addElement('header', 'headerfilestoprocess', get_string('scanoptions', VPL));
-        $options=array();
-        for($i=5; $i<=40; $i+=5){
-        	$options[$i]=$i;
+        if (count( $filelist ) > 0) {
+            $mform->addElement( 'header', 'headerfilestoprocess', get_string( 'filestoscan', VPL ) );
+            $num = 0;
+            foreach ($filelist as $filename) {
+                $entry = 'file' . $num;
+                $mform->addElement( 'checkbox', $entry, $filename );
+                $mform->setDefault( $entry, true );
+                $mform->disabledIf( $entry, 'allfiles', 'checked' );
+                $num ++;
+            }
+            $mform->addElement( 'checkbox', 'allfiles', get_string( 'allfiles', VPL ) );
+        } else {
+            $mform->addElement( 'hidden', 'allfiles', 'checked' );
+            $mform->setType( 'allfiles', PARAM_BOOL );
         }
-        for($i=60; $i<=100; $i+=20){
-        	$options[$i]=$i;
-        }
-        for($i=150; $i<=400; $i+=50){
-        	$options[$i]=$i;
-        }
-        $mform->addElement('select', 'maxoutput', get_string('maxsimilarityoutput',VPL), $options);
-        $mform->setType('maxoutput', PARAM_INT);
-        $cid=$this->vpl->get_course()->id;
-		$mform->addElement('header', 'headerothersources', get_string('othersources', VPL));
-        $mform->addElement('select', 'scanactivity', get_string('scanactivity',VPL),$this->list_activities($this->vpl->get_instance()->id));
-        $mform->addElement('filepicker', 'scanzipfile0', get_string('scanzipfile', VPL));
-        //TODO match 2.X? 
-        //$mform->addElement('filepicker', 'scanzipfile1', get_string('scanzipfile', VPL));
-        //$mform->addElement('select', 'scandirectory', get_string('scandirectory', VPL), $this->list_directories($cid));
-        $mform->addElement('checkbox','searchotherfiles',get_string('scanother',VPL));
-        $mform->setDefault('searchotherfiles', false);
-        $this->add_action_buttons(false,get_string('search'));
-	}
+        $mform->addElement( 'checkbox', 'joinedfiles', get_string( 'joinedfiles', VPL ) );
+        $cid = $this->vpl->get_course()->id;
+        $mform->addElement( 'header', 'headerothersources', get_string( 'othersources', VPL ) );
+        $mform->addElement( 'select', 'scanactivity', get_string( 'scanactivity', VPL )
+                           , $this->list_activities( $this->vpl->get_instance()->id ) );
+        $mform->addElement( 'filepicker', 'scanzipfile0', get_string( 'scanzipfile', VPL ) );
+        $mform->addElement( 'checkbox', 'searchotherfiles', get_string( 'scanother', VPL ) );
+        $mform->setDefault( 'searchotherfiles', false );
+        $this->add_action_buttons( false, get_string( 'search' ) );
+    }
 }
-?>
--- similarity/similarity_html.class.php
+++ similarity/similarity_html.class.php
@@ -15,31 +15,23 @@
 // along with VPL for Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Similarity form
+ * HTML similarity class
  *
  * @package mod_vpl
- * @copyright 2012 Juan Carlos Rodríguez-del-Pino
+ * @copyright 2012 onwards Juan Carlos Rodríguez-del-Pino
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  * @author Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
  */
+require_once(dirname(__FILE__).'/similarity_base.class.php');
 
-require_once(dirname(__FILE__).'/../../../config.php');
-require_once(dirname(__FILE__).'/../locallib.php');
-require_once(dirname(__FILE__).'/../vpl.class.php');
-require_once(dirname(__FILE__).'/../vpl_submission.class.php');
-require_once($CFG->libdir.'/formslib.php');
-require_once(dirname(__FILE__).'/similarity_form.class.php');
-$id = required_param( 'id', PARAM_INT );
-$vpl = new mod_vpl( $id );
-$vpl->prepare_page( 'similarity/similarity_form.php', array (
-        'id' => $id
-) );
-
-$vpl->require_capability( VPL_SIMILARITY_CAPABILITY );
-\mod_vpl\event\vpl_similarity_form_viewed::log( $vpl );
-//Print header
-$vpl->print_header( get_string( 'similarity', VPL ) );
-$vpl->print_view_tabs( basename( __FILE__ ) );
-$form = new vpl_similarity_form( 'listsimilarity.php', $vpl );
-$form->display();
-$vpl->print_footer();
+class vpl_similarity_html extends vpl_similarity_base {
+    public function get_type() {
+        return 9;
+    }
+    public function sintax_normalize(&$tokens) {
+        return $tokens;
+    }
+    public function get_tokenizer() {
+        return vpl_tokenizer_factory::get( 'html' );
+    }
+}
