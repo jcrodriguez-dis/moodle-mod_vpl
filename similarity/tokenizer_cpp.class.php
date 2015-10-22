2da4dbe266a2873de9b3c51c62c60b051f04e965
Juan Carlos Rodriguez-del-Pino <jcrodriguez@dis.ulpgc.es>
2015-10-22 20:25:20 +0100

    Uses Moodle code style

--- similarity/tokenizer_cpp.class.php
+++ similarity/tokenizer_cpp.class.php
@@ -1,28 +1,74 @@
 <?php
+// This file is part of VPL for Moodle - http://vpl.dis.ulpgc.es/
+//
+// VPL for Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// VPL for Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with VPL for Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
- * @version		$Id: tokenizer_cpp.class.php,v 1.5 2012-06-05 23:22:11 juanca Exp $
- * @package		VPL. C++ programing language tokenizer class
- * @copyright	2012 Juan Carlos Rodríguez-del-Pino
- * @license		http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- * @author		Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
+ * C++ programing language tokenizer class
+ *
+ * @package mod_vpl
+ * @copyright 2012 Juan Carlos Rodríguez-del-Pino
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
  */
 
-require_once dirname(__FILE__).'/tokenizer_c.class.php';
+require_once(dirname(__FILE__).'/tokenizer_c.class.php');
 
-class vpl_tokenizer_cpp extends vpl_tokenizer_c{
-	function __construct(){
-		parent::__construct();
-		$added = array( 'and' => true, 'and_eq' => true, 'bitand' => true, 'bitor' => true,
-					 'bool' => true, 'catch' => true, 'class' => true, 'compl' => true,
-					 'const_cast' => true, 'delete' => true, 'dynamic_cast' => true, 'explicit' => true,
- 					 'export' => true, 'false' => true, 'friend' => true, 'inline' => true,
- 					 'namespace' => true, 'new' => true, 'not' => true, 'not_eq' => true, 'operator' => true,
- 					 'or' => true, 'or_eq' => true, 'private' => true, 'protected' => true, 'public' => true,
- 					 'reinterpret_cast' => true, 'static_cast' => true, 'template' => true, 'this' => true,
- 					 'throw' => true, 'true' => true, 'try' => true, 'typeid' => true, 'typename' => true,
-					 'using' => true, 'virtual' => true, 'xor' => true, 'xor_eq' => true);
-		$this->reserved= array_merge($this->reserved, $added);
-	}
+class vpl_tokenizer_cpp extends vpl_tokenizer_c {
+    public function __construct() {
+        parent::__construct();
+        $added = array (
+                'and' => true,
+                'and_eq' => true,
+                'bitand' => true,
+                'bitor' => true,
+                'bool' => true,
+                'catch' => true,
+                'class' => true,
+                'compl' => true,
+                'const_cast' => true,
+                'delete' => true,
+                'dynamic_cast' => true,
+                'explicit' => true,
+                'export' => true,
+                'false' => true,
+                'friend' => true,
+                'inline' => true,
+                'namespace' => true,
+                'new' => true,
+                'not' => true,
+                'not_eq' => true,
+                'operator' => true,
+                'or' => true,
+                'or_eq' => true,
+                'private' => true,
+                'protected' => true,
+                'public' => true,
+                'reinterpret_cast' => true,
+                'static_cast' => true,
+                'template' => true,
+                'this' => true,
+                'throw' => true,
+                'true' => true,
+                'try' => true,
+                'typeid' => true,
+                'typename' => true,
+                'using' => true,
+                'virtual' => true,
+                'xor' => true,
+                'xor_eq' => true
+        );
+        $this->reserved = array_merge( $this->reserved, $added );
+    }
 }
-
-?>
--- similarity/tokenizer_factory.class.php
+++ similarity/tokenizer_factory.class.php
@@ -1,22 +1,37 @@
 <?php
+// This file is part of VPL for Moodle - http://vpl.dis.ulpgc.es/
+//
+// VPL for Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// VPL for Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with VPL for Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
- * @version		$Id: tokenizer_factory.class.php,v 1.4 2012-06-05 23:22:10 juanca Exp $
- * @package		VPL. Tokenizer factory class
- * @copyright	2012 Juan Carlos Rodríguez-del-Pino
- * @license		http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- * @author		Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
+ * Tokenizer factory class
+ *
+ * @package mod_vpl
+ * @copyright 2012 Juan Carlos Rodríguez-del-Pino
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
  */
 
-class vpl_tokenizer_factory{
-	static private $classloaded=array();
-	static function get($type){
-		if(!isset(self::$classloaded[$type])){
-			$include = 'tokenizer_'.$type.'.class.php';
-			require_once dirname(__FILE__).'/'.$include;
-			$class = 'vpl_tokenizer_'.$type;
-			self::$classloaded[$type] = new $class();
-		}
-		return self::$classloaded[$type];
-	}
+class vpl_tokenizer_factory {
+    private static $classloaded = array ();
+    static public function get($type) {
+        if (! isset( self::$classloaded [$type] )) {
+            $include = 'tokenizer_' . $type . '.class.php';
+            require_once(dirname( __FILE__ ) . '/' . $include);
+            $class = 'vpl_tokenizer_' . $type;
+            self::$classloaded [$type] = new $class();
+        }
+        return self::$classloaded [$type];
+    }
 }
-?>
