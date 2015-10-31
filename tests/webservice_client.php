1927e14d4c815006d7e70673504460a2139fa54b
Juan Carlos Rodriguez-del-Pino <jcrodriguez@dis.ulpgc.es>
2015-10-31 13:59:05 +0000

    Uses Moodle code style

--- tests/webservice_client.php
+++ tests/webservice_client.php
@@ -1,12 +1,29 @@
 <?php
+// This file is part of VPL for Moodle - http://vpl.dis.ulpgc.es/
+//
+// VPL for Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// VPL for Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with VPL for Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
- * @package		VPL. Example of VPL web service client
- * @copyright	2014 Juan Carlos Rodríguez-del-Pino
- * @license		http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- * @author		Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
+ * Example of VPL web service client
+ *
+ * @package mod_vpl.
+ * @copyright 2014 Juan Carlos Rodríguez-del-Pino
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
  */
 
-/*********** DOCUMENTATION ************ 
+/*********** DOCUMENTATION ************
 * WARNING *
   You need to add manually to the URL of this script
 the id of one VPL activity to be tested
@@ -26,7 +43,7 @@ To be available in a Moodle server, need be enabled:
 will appear a  link to "Web service". Clicking this link will be
 shown a page that gives us a URL to the VPL web service. This
 URL must be copy and paste into the client by the user. This URL
-content the token and the id of the VPL activity to be accessed. 
+content the token and the id of the VPL activity to be accessed.
 
 * DEVELOPMENT OF WEB SERVICE CLIENTS *
   The service has been developed using the web service API of Moodle.
@@ -53,100 +70,104 @@ a permanent token, in this case, you need to generate the
 correct URL to the standard protocol server.
 *********** END DOCUMENTATION ************/
 
-require_once dirname(__FILE__).'/../../../config.php';
+require_once(dirname(__FILE__).'/../../../config.php');
+require_once(dirname( __FILE__ ) . '/../vpl.class.php');
+
+function vpl_call_service($url, $fun, $request = '') {
+    if (! function_exists( 'curl_init' )) {
+        throw new Exception( 'PHP cURL requiered' );
+    }
+    $ch = curl_init();
+    curl_setopt( $ch, CURLOPT_URL, $url . $fun );
+    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+    curl_setopt( $ch, CURLOPT_POST, 1 );
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: text/urlencode;charset=UTF-8'));
+    curl_setopt( $ch, CURLOPT_POSTFIELDS, $request );
+    curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, 5 );
+    if (@$CFG->vpl_acceptcertificates) {
+        curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
+    }
+    $rawresponse = curl_exec( $ch );
+    if ($rawresponse === false) {
+        $error = 'request failed: ' . s( curl_error( $ch ) );
+        curl_close( $ch );
+        return $error;
+    } else {
+        curl_close( $ch );
+        return json_decode( $rawresponse );
+    }
+}
+function vpl_call_print($res) {
+    echo '<pre>';
+    p( $res );
+    echo '</pre>';
+}
 
 require_login();
 
-require_once dirname(__FILE__).'/../vpl.class.php';
-
-function vpl_call_service($url,$fun,$request=''){
-	if(!function_exists('curl_init')){
-		throw new Exception('PHP cURL requiered');
-	}
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $url.$fun);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-	curl_setopt($ch, CURLOPT_POST, 1);
-	//curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: text/urlencode;charset=UTF-8'));
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $request);
-	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-	if( @$CFG->vpl_acceptcertificates )
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	$raw_response=curl_exec($ch);
-	if($raw_response === false){
-		$error='request failed: '.s(curl_error($ch));
-		curl_close($ch);
-		return $error;
-	}else{
-		curl_close($ch);
-		return json_decode($raw_response);
-	}
-}
-
-function vpl_call_print($res){
-	echo '<pre>';
-	s(print_r($res,false));
-	echo '</pre>';
-}
-
-$id = required_param('id',PARAM_INT);
-$vpl = new mod_vpl($id);
-$vpl->require_capability(VPL_MANAGE_CAPABILITY);
-$vpl->prepare_page('tests/webservice_client.php', array('id' => $id));
-$vpl->print_header('Web service test client');
+$id = required_param( 'id', PARAM_INT );
+$vpl = new mod_vpl( $id );
+$vpl->require_capability( VPL_MANAGE_CAPABILITY );
+$vpl->prepare_page( 'tests/webservice_client.php', array (
+        'id' => $id
+) );
+$vpl->print_header( 'Web service test client' );
 echo '<h1>Web service test client</h1>';
 echo '<h3>Session token generated (or reused)</h3>';
-echo s(vpl_get_webservice_token($vpl));
-$service_url = vpl_get_webservice_urlbase($vpl);
+echo s( vpl_get_webservice_token( $vpl ) );
+$serviceurl = vpl_get_webservice_urlbase( $vpl );
 echo '<h3>Base URL for web service</h3>';
-echo s($service_url);
+echo s( $serviceurl );
 echo '<h3>Get info from activity</h3>';
-$res=vpl_call_service($service_url,'mod_vpl_info');
-vpl_call_print($res);
+$res = vpl_call_service( $serviceurl, 'mod_vpl_info' );
+vpl_call_print( $res );
 echo '<h3>Get last submission</h3>';
-$res=vpl_call_service($service_url,'mod_vpl_open');
-vpl_call_print($res);
+$res = vpl_call_service( $serviceurl, 'mod_vpl_open' );
+vpl_call_print( $res );
 echo '<h3>Modify and save last submission</h3>';
-if(isset($res->files)){
-	$files=$res->files;
+if (isset( $res->files )) {
+    $files = $res->files;
 }
-if(count($files)==0){
-	$file = new stdClass();
-	$file->name = 'test.c';
-	$file->data = 'int main(){printf("hello");}';
-	$files = array($file);
-}else{
-	foreach($files as $file){
-		$file->data="Modification ".time()."\n".$file->data;
-	}
+if (count( $files ) == 0) {
+    $file = new stdClass();
+    $file->name = 'test.c';
+    $file->data = 'int main(){printf("hello");}';
+    $files = array (
+            $file
+    );
+} else {
+    foreach ($files as $file) {
+        $file->data = "Modification " . time() . "\n" . $file->data;
+    }
 }
-$res->files=$files;
-$body='';
-foreach($files as $key => $file){
-	if($key>0)
-		$body.='&';
-	$body.="files[$key][name]=".urlencode($file->name).'&';
-	$body.="files[$key][data]=".urlencode($file->data);
+$res->files = $files;
+$body = '';
+foreach ($files as $key => $file) {
+    if ($key > 0) {
+        $body .= '&';
+    }
+    $body .= "files[$key][name]=" . urlencode( $file->name ) . '&';
+    $body .= "files[$key][data]=" . urlencode( $file->data );
 }
 
-$newres=vpl_call_service($service_url,'mod_vpl_save',$body);
-vpl_call_print($newres);
+$newres = vpl_call_service( $serviceurl, 'mod_vpl_save', $body );
+vpl_call_print( $newres );
 echo '<h3>Reread file to test saved files</h3>';
-$newres=vpl_call_service($service_url,'mod_vpl_open');
-if(!isset($res->files) or !isset($newres->files) or $res->files != $newres->files){
-	echo "Error";
-}else{
-	echo "OK";
+$newres = vpl_call_service( $serviceurl, 'mod_vpl_open' );
+if (! isset( $res->files ) or ! isset( $newres->files ) or $res->files != $newres->files) {
+    echo "Error";
+} else {
+    echo "OK";
 }
-vpl_call_print($newres);
+vpl_call_print( $newres );
 
 echo '<h3>Call evaluate (unreliable test)</h3>';
 echo '<h4>It may be unavailable</h4>';
 echo '<h4>The client don\'t use websocket then the jail server may timeout</h4>';
-$res=vpl_call_service($service_url,'mod_vpl_evaluate');
-vpl_call_print($res);
-sleep(5);
+$res = vpl_call_service( $serviceurl, 'mod_vpl_evaluate' );
+vpl_call_print( $res );
+sleep( 5 );
 echo '<h3>Call get result of last evaluation (unreliable test)</h3>';
-$res=vpl_call_service($service_url,'mod_vpl_get_result');
-vpl_call_print($res);
+$res = vpl_call_service( $serviceurl, 'mod_vpl_get_result' );
+vpl_call_print( $res );
 $vpl->print_footer();
