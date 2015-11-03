63c1662da14169144de16bbe68b6c6c8db6cf4ae
Juan Carlos Rodriguez-del-Pino <jcrodriguez@dis.ulpgc.es>
2015-11-03 16:51:38 +0000

    Uses Moodle code style

--- views/sh_factory.class.php
+++ views/sh_factory.class.php
@@ -1,60 +1,80 @@
 <?php
+// This file is part of VPL for Moodle - http://vpl.dis.ulpgc.es/
+//
+// VPL for Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// VPL for Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with VPL for Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
- * @package		VPL. Syntaxhighlighters object factory class
- * @copyright	2012 onwards Juan Carlos Rodríguez-del-Pino
- * @license		http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- * @author		Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
+ * VPL Syntaxhighlighter object factory class
+ *
+ * @package mod_vpl
+ * @copyright 2012 onwards Juan Carlos Rodríguez-del-Pino
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
  */
 
-class vpl_sh_factory{
-	static $cache = array();
-	static function get_object($type){
-		if(!isset(self::$cache[$type])){
-			require_once dirname(__FILE__).'/sh_'.$type.'.class.php';
-			$class = 'vpl_sh_'.$type;
-			self::$cache[$type] = new $class();
-		}
-		return self::$cache[$type];
-	}
-
-	static function get_sh($filename){
-		$ext = vpl_fileExtension($filename);
-		if(vpl_is_binary($filename)){
-			if(vpl_is_image($filename)){
-				return self::get_object('image');
-			}else{
-				return self::get_object('binary');
-			}
-		}
-		if($ext == 'c'){
-			return self::get_object('c');
-		}elseif($ext == 'cpp' || $ext == 'h'){
-			return self::get_object('cpp');
-		}elseif($ext == 'java'){
-			return self::get_object('java');
-		}elseif($ext == 'ada' || $ext == 'adb' || $ext == 'ads'){
-			return self::get_object('ada');
-		}elseif($ext == 'sql'){
-			return self::get_object('sql');
-		}elseif($ext == 'scm'){
-			return self::get_object('scheme');
-		}elseif($ext == 'sh'){
-			return self::get_object('sh');
-		}elseif($ext == 'pas'){
-			return self::get_object('pascal');
-		}elseif($ext == 'f77' || $ext == 'f' ){
-			return self::get_object('fortran77');
-		}elseif($ext == 'pl' ){
-			return self::get_object('prolog');
-		}elseif($ext == 'm' ){
-			return self::get_object('matlab');
-		}elseif($ext == 'py' ){
-			return self::get_object('python');
-		}elseif($ext == 'scala' ){
-			return self::get_object('scala');
-		}else{
-			return self::get_object('geshi');
-		}
-	}
+class vpl_sh_factory {
+    protected static $cache = array ();
+    public static function get_object($type) {
+        if (! isset( self::$cache [$type] )) {
+            require_once(dirname( __FILE__ ) . '/sh_' . $type . '.class.php');
+            $class = 'vpl_sh_' . $type;
+            self::$cache [$type] = new $class();
+        }
+        return self::$cache [$type];
+    }
+    public static function get_sh($filename) {
+        $ext = vpl_fileextension( $filename );
+        if (vpl_is_binary( $filename )) {
+            if (vpl_is_image( $filename )) {
+                return self::get_object( 'image' );
+            } else {
+                return self::get_object( 'binary' );
+            }
+        }
+        switch ($ext) {
+            case 'c' :
+                return self::get_object( 'c' );
+            case 'cpp' :
+            case 'h' :
+                return self::get_object( 'cpp' );
+            case 'java' :
+                return self::get_object( 'java' );
+            case 'ada' :
+            case 'adb' :
+            case 'ads' :
+                return self::get_object( 'ada' );
+            case 'sql' :
+                return self::get_object( 'sql' );
+            case 'scm' :
+                return self::get_object( 'scheme' );
+            case 'sh' :
+                return self::get_object( 'sh' );
+            case 'pas' :
+                return self::get_object( 'pascal' );
+            case 'f77' :
+            case 'f' :
+                return self::get_object( 'fortran77' );
+            case 'pl' :
+                return self::get_object( 'prolog' );
+            case 'm' :
+                return self::get_object( 'matlab' );
+            case 'py' :
+                return self::get_object( 'python' );
+            case 'scala' :
+                return self::get_object( 'scala' );
+            default :
+                return self::get_object( 'geshi' );
+        }
+    }
 }
-?>
--- views/sh_fortran77.class.php
+++ views/sh_fortran77.class.php
@@ -1,177 +1,239 @@
 <?php
+// This file is part of VPL for Moodle - http://vpl.dis.ulpgc.es/
+//
+// VPL for Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// VPL for Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with VPL for Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
- * @package		VPL. Syntaxhighlighter for Fortran77 language
- * @copyright	2012 onwards Juan Carlos Rodríguez-del-Pino
- * @license		http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- * @author		Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
+ * VPL Syntaxhighlighter for Fortran77 language
+ *
+ * @package mod_vpl
+ * @copyright 2012 onwards Juan Carlos Rodríguez-del-Pino
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
  */
- 
-require_once dirname(__FILE__).'/sh_text.class.php';
 
-class vpl_sh_fortran77 extends vpl_sh_text{
-	var $previous_pending;
-	protected function show_pending(&$rest){
-		$lower=strtolower($rest);
-		if(array_key_exists($lower , $this->reserved)){
-			$this->initTag(self::c_reserved);
-			parent::show_pending($rest);
-			echo self::endTag;
-		}else{
-			parent::show_pending($rest);
-		}
-		$this->previous_pending=$lower;
-		$rest ='';
-	}
-	protected function is_begin_identifier($c){
-		return ($c >= 'a' && $c <= 'z') || ($c >= 'A' && $c <= 'Z')
-		|| ($c >= '0' && $c <= '9') || ($c == '$') || ($c == '_');
-	}
-	function __construct(){
-		$this->reserved= array('accept' => 1, 'assign' => 1,	'backspace' => 1,
-					'call' => 1,	'close' => 1, 'continue' => 1,
-					'decode' => 1, 'do' => 1,	'dowhile' => 1,
-					'else' => 1, 'elseif' => 1, 'encode' => 1,
-					'enddo' => 1, 'endfile' => 1,	'endif' => 1,
-					'goto' => 1,	'if' => 1, 'include' => 1,
-					'inquire' => 1, 'open' => 1, 'pause' => 1,
-					'print' => 1, 'return' => 1, 'rewind' => 1,
-					'save' => 1, 'static' => 1, 'stop' => 1,
-					'write' => 1,
-					//From here declarators
-					'automatic' => 1, 'blockdata' => 1, 'byte' => 1, 'character' => 1,
-					'common' => 1, 'complex' => 1, 'data' => 1, 'dimension' => 1, 'doublecomplex' => 1,
-					'doubleprecision' => 1, 'end' => 1, 'endmap' => 1, 'endstructure' => 1,	'endunion' => 1,
-					'equivalence' => 1, 'external' => 1, 'format' => 1, 'function' => 1,
-					'implicit' => 1,	'integer' => 1, 'intrinsic' => 1, 'logical' => 1,
-					'map' => 1, 'namelist' => 1, 'options' => 1, 'parameter' => 1,
-					'pointer' => 1, 'pragma' => 1, 'program' => 1, 'real' => 1,
-					'record' => 1, 'static' => 1, 'structure' => 1, 'subroutine' => 1,
-					'type' => 1, 'union' => 1, 'virtual' => 1, 'volatile' => 1
-		);
-		parent::__construct();
-	}
-	const in_regular = 0;
-	const in_string = 1;
-	const in_cstring = 2;
-	const in_dstring = 3;
-	const in_linecomment = 4;
-	const in_identifier = 5;
-	function print_file($filename, $filedata, $showln=true){
-		$this->begin($filename,$showln);
-		$state=self::in_regular;
-		$pending='';
-		$l = strlen($filedata);
-		if($l){
-			$this->show_line_number();
-		}
-		$current='';
-		for($i=0;$i<$l;$i++){
-			$current=$filedata[$i];
-			if($i < ($l-1)) {
-				$next = $filedata[$i+1];
-			}else{
-				$next ='';
-			}
-			if($current == self::CR){
-				if($next == self::LF) {
-					continue;
-				}else{
-					$current = self::LF;
-				}
-			}
+require_once(dirname(__FILE__).'/sh_text.class.php');
 
-			switch($state){
-				case self::in_regular:
-				case self::in_identifier:
-					if ($current == '#') {
-						$this->show_pending($pending);
-						$state = self::in_linecomment;
-						$this->initTag(self::c_comment);
-					}  elseif ($current == '\'') {
-						$this->show_pending($pending);
-						$this->initTag(self::c_string);
-						$state=self::in_string;
-					} elseif ($current == '"') {
-						$this->show_pending($pending);
-						$state=self::in_dstring;
-						$this->initTag(self::c_string);
-					} elseif ($current == '$' && $next == '\'') {
-						$this->show_pending($pending);
-						$pending = '$\'';
-						$i++;
-						$this->initTag(self::c_string);
-						$state=self::in_cstring;
-						continue 2;
-					} elseif ($this->is_begin_identifier($current)) {
-						if ($state==self::in_regular){
-							$this->show_pending($pending);
-							$state = self::in_identifier;
-						}
-					} elseif($state==self::in_identifier){
-						$this->show_pending($pending);
-						$state=self::in_regular;
-					}
-					break;
-				case self::in_linecomment:
-					if ($current == self::LF){
-						$this->show_pending($pending);
-						echo "\n";
-						$this->endTag();
-						$this->show_line_number();
-						$state=self::in_regular;
-						continue 2;
-					}
-				case self::in_string:
-					if ($current == '\'') {
-						$pending .= '\'';
-						$this->show_pending($pending);
-						$this->endTag();
-						$state = self::in_regular;
-						continue 2;
-					}
-				break;
-				case self::in_cstring:
-					if ($current == '\'') {
-						$pending .= '\'';
-						$this->show_pending($pending);
-						$this->endTag();
-						$state = self::in_regular;
-						continue 2;
-					}
-					if ($current == '\\') { //Jump next
-						$pending .= '\\'.$next;
-						$i++;
-						continue 2;
-					}
-				break;
-				case self::in_dstring:
-					if ($current == '"') {
-						$pending .= '"';
-						$this->show_pending($pending);
-						$this->endTag();
-						$state = self::in_regular;
-						continue 2;
-					}
-					if ($current == '\\') { //Jump next
-						$pending .= '\\'.$next;
-						$i++;
-						continue 2;
-					}
-				break;
-			}
-			$pending .= $current;
-			if($current == self::LF){
-				$this->show_pending($pending);
-				$this->show_line_number();
-			}
-		}
+class vpl_sh_fortran77 extends vpl_sh_text {
+    protected $previouspending;
+    protected function show_pending(&$rest) {
+        $lower = strtolower( $rest );
+        if (array_key_exists( $lower, $this->reserved )) {
+            $this->initTag( self::c_reserved );
+            parent::show_pending( $rest );
+            echo self::endTag;
+        } else {
+            parent::show_pending( $rest );
+        }
+        $this->previouspending = $lower;
+        $rest = '';
+    }
+    protected function is_begin_identifier($c) {
+        return ($c >= 'a' && $c <= 'z') || ($c >= 'A' && $c <= 'Z') || ($c >= '0' && $c <= '9') || ($c == '$') || ($c == '_');
+    }
+    public function __construct() {
+        $this->reserved = array (
+                'accept' => 1,
+                'assign' => 1,
+                'backspace' => 1,
+                'call' => 1,
+                'close' => 1,
+                'continue' => 1,
+                'decode' => 1,
+                'do' => 1,
+                'dowhile' => 1,
+                'else' => 1,
+                'elseif' => 1,
+                'encode' => 1,
+                'enddo' => 1,
+                'endfile' => 1,
+                'endif' => 1,
+                'goto' => 1,
+                'if' => 1,
+                'include' => 1,
+                'inquire' => 1,
+                'open' => 1,
+                'pause' => 1,
+                'print' => 1,
+                'return' => 1,
+                'rewind' => 1,
+                'save' => 1,
+                'static' => 1,
+                'stop' => 1,
+                'write' => 1,
+                /* From here declarators. */
+                'automatic' => 1,
+                'blockdata' => 1,
+                'byte' => 1,
+                'character' => 1,
+                'common' => 1,
+                'complex' => 1,
+                'data' => 1,
+                'dimension' => 1,
+                'doublecomplex' => 1,
+                'doubleprecision' => 1,
+                'end' => 1,
+                'endmap' => 1,
+                'endstructure' => 1,
+                'endunion' => 1,
+                'equivalence' => 1,
+                'external' => 1,
+                'format' => 1,
+                'function' => 1,
+                'implicit' => 1,
+                'integer' => 1,
+                'intrinsic' => 1,
+                'logical' => 1,
+                'map' => 1,
+                'namelist' => 1,
+                'options' => 1,
+                'parameter' => 1,
+                'pointer' => 1,
+                'pragma' => 1,
+                'program' => 1,
+                'real' => 1,
+                'record' => 1,
+                'static' => 1,
+                'structure' => 1,
+                'subroutine' => 1,
+                'type' => 1,
+                'union' => 1,
+                'virtual' => 1,
+                'volatile' => 1
+        );
+        parent::__construct();
+    }
+    const IN_REGULAR = 0;
+    const IN_STRING = 1;
+    const IN_CSTRING = 2;
+    const IN_DSTRING = 3;
+    const IN_LINECOMMENT = 4;
+    const IN_IDENTIFIER = 5;
+    public function print_file($filename, $filedata, $showln = true) {
+        $this->begin( $filename, $showln );
+        $state = self::IN_REGULAR;
+        $pending = '';
+        $l = strlen( $filedata );
+        if ($l) {
+            $this->show_line_number();
+        }
+        $current = '';
+        for ($i = 0; $i < $l; $i ++) {
+            $current = $filedata [$i];
+            if ($i < ($l - 1)) {
+                $next = $filedata [$i + 1];
+            } else {
+                $next = '';
+            }
+            if ($current == self::CR) {
+                if ($next == self::LF) {
+                    continue;
+                } else {
+                    $current = self::LF;
+                }
+            }
 
-		$this->show_pending($pending);
-		if($state != self::in_regular){
-			$this->endTag();
-		}
-		$this->end();
-	}
+            switch ($state) {
+                case self::IN_REGULAR :
+                case self::IN_IDENTIFIER :
+                    if ($current == '#') {
+                        $this->show_pending( $pending );
+                        $state = self::IN_LINECOMMENT;
+                        $this->initTag( self::c_comment );
+                    } else if ($current == '\'') {
+                        $this->show_pending( $pending );
+                        $this->initTag( self::c_string );
+                        $state = self::IN_STRING;
+                    } else if ($current == '"') {
+                        $this->show_pending( $pending );
+                        $state = self::IN_DSTRING;
+                        $this->initTag( self::c_string );
+                    } else if ($current == '$' && $next == '\'') {
+                        $this->show_pending( $pending );
+                        $pending = '$\'';
+                        $i ++;
+                        $this->initTag( self::c_string );
+                        $state = self::IN_CSTRING;
+                        continue 2;
+                    } else if ($this->is_begin_identifier( $current )) {
+                        if ($state == self::IN_REGULAR) {
+                            $this->show_pending( $pending );
+                            $state = self::in_identifier;
+                        }
+                    } else if ($state == self::in_identifier) {
+                        $this->show_pending( $pending );
+                        $state = self::IN_REGULAR;
+                    }
+                    break;
+                case self::IN_LINECOMMENT :
+                    if ($current == self::LF) {
+                        $this->show_pending( $pending );
+                        echo "\n";
+                        $this->endTag();
+                        $this->show_line_number();
+                        $state = self::IN_REGULAR;
+                        continue 2;
+                    }
+                case self::IN_STRING :
+                    if ($current == '\'') {
+                        $pending .= '\'';
+                        $this->show_pending( $pending );
+                        $this->endTag();
+                        $state = self::IN_REGULAR;
+                        continue 2;
+                    }
+                    break;
+                case self::IN_CSTRING :
+                    if ($current == '\'') {
+                        $pending .= '\'';
+                        $this->show_pending( $pending );
+                        $this->endTag();
+                        $state = self::IN_REGULAR;
+                        continue 2;
+                    }
+                    if ($current == '\\') { // Jump next.
+                        $pending .= '\\' . $next;
+                        $i ++;
+                        continue 2;
+                    }
+                    break;
+                case self::IN_DSTRING :
+                    if ($current == '"') {
+                        $pending .= '"';
+                        $this->show_pending( $pending );
+                        $this->endTag();
+                        $state = self::IN_REGULAR;
+                        continue 2;
+                    }
+                    if ($current == '\\') { // Jump next.
+                        $pending .= '\\' . $next;
+                        $i ++;
+                        continue 2;
+                    }
+                    break;
+            }
+            $pending .= $current;
+            if ($current == self::LF) {
+                $this->show_pending( $pending );
+                $this->show_line_number();
+            }
+        }
+
+        $this->show_pending( $pending );
+        if ($state != self::IN_REGULAR) {
+            $this->endTag();
+        }
+        $this->end();
+    }
 }
-
-?>
