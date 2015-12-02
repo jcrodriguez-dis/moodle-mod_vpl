26d19d70fb437f0d17b68dd925602128c98fe8fb
Juan Carlos Rodriguez-del-Pino <jcrodriguez@dis.ulpgc.es>
2015-12-02 12:48:37 +0000

    Add support for tablets and clipboard

--- editor/VPLTerminal.js
+++ editor/VPLTerminal.js
@@ -23,6 +23,58 @@
  */
 
 // Terminal constructor
+VPL_Clipboard = function(dialog_id, hlabel1, action1, hlabel2, action2, onFocus){
+    var tdialog = $JQVPL('#' + dialog_id);
+    var label1 = tdialog.find('.vpl_clipboard_label1');
+    var label2 = tdialog.find('.vpl_clipboard_label2');
+    var entry1 = tdialog.find('.vpl_clipboard_entry1');
+    var entry2 = tdialog.find('.vpl_clipboard_entry2');
+    label1.html(hlabel1);
+    label2.html(hlabel2);
+    if(action1) {
+        label1.button().click(action1);
+    }
+    if(action2) {
+        label2.button().click(action2);
+    }
+    tdialog.dialog({
+        title : VPL_Util.str('clipboard'),
+        closeOnEscape : true,
+        autoOpen : false,
+        width : 'auto',
+        height : 'auto',
+        resizable : true,
+        dialogClass : 'vpl_ide',
+    });
+    if ( onFocus ){
+        tdialog.on( "click",onFocus);
+    }
+    this.show = function() {
+        tdialog.dialog('open');
+    };
+    this.hide = function() {
+        tdialog.dialog('close');
+    };
+    this.setEntry1 = function(v) {
+        entry1.val(v);
+        entry1.select();
+    };
+    this.getEntry1 = function() {
+        return entry1.val();
+    };
+    this.setEntry2 = function(v) {
+        entry2.val(v);
+    };
+    this.getEntry2 = function() {
+        return entry2.val();
+    };
+    var titleTag = tdialog.siblings().find('.ui-dialog-title');
+    var clipboardTitle =  VPL_Util.gen_icon('clipboard','sw');
+    clipboardTitle +=  ' '+VPL_Util.str('clipboard');
+    titleTag.html(clipboardTitle);
+    tdialog.parent().css('overflow',''); //Fix problem with JQuery
+};
+
 VPL_Terminal = function(dialog_id, terminal_id, str) {
     var self = this;
     var ws;
@@ -31,7 +83,12 @@ VPL_Terminal = function(dialog_id, terminal_id, str) {
     var title = '';
     var message = '';
     var tdialog = $JQVPL('#' + dialog_id);
-
+    var titleText;
+    var clipboard = null;
+    var cliboardMaxsize = 64000;
+    var clipboardData = '';
+    var textWritten = '';
+    
     var terminal = new Terminal({
         cols : 80,
         rows : 24,
@@ -41,6 +98,7 @@ VPL_Terminal = function(dialog_id, terminal_id, str) {
     terminal.on('data', function(data) {
         if (ws && ws.readyState == ws.OPEN) {
             ws.send(data);
+            textWritten += data;
         }
     });
     var terminal_tag = $JQVPL('#' + terminal_id);
@@ -49,7 +107,7 @@ VPL_Terminal = function(dialog_id, terminal_id, str) {
         if (message != '') {
             text += ' (' + message + ')';
         }
-        tdialog.dialog("option", "title", str('console') + ": " + text);
+        titleText.text(str('console') + ": " + text);
     };
     this.setTitle = function(t) {
         title = t;
@@ -58,6 +116,25 @@ VPL_Terminal = function(dialog_id, terminal_id, str) {
     this.setMessage = function(t) {
         message = t;
         this.updateTitle();
+    };
+    function receiveClipboard(data) {
+        clipboardData += data;
+        if(clipboardData.length > cliboardMaxsize){
+            var from=clipboardData.length - cliboardMaxsize/2;
+            clipboardData = clipboardData.substr(from);
+        }
+    };
+    function pasteClipboard() {
+        if (ws && ws.readyState == ws.OPEN) {
+            ws.send(clipboard.getEntry2());
+        }
+    };
+    function updateClipboard() {
+        clipboard.setEntry1(clipboardData);
+    };
+    function openClipboard() {
+        updateClipboard();
+        clipboard.show();
     };
 
     this.connect = function(server, onClose) {
@@ -68,12 +145,14 @@ VPL_Terminal = function(dialog_id, terminal_id, str) {
             if (ws) {
                 ws.close();
             }
+            clipboardData = '';
             self.setMessage('');
             self.setTitle(str('connecting'));
             ws = new WebSocket(server);
             ws.writeBuffer = '';
             ws.writeIt = function() {
                 terminal.write(ws.writeBuffer);
+                receiveClipboard(ws.writeBuffer);
                 ws.writeBuffer = '';
             };
             ws.onmessage = function(event) {
@@ -110,9 +189,13 @@ VPL_Terminal = function(dialog_id, terminal_id, str) {
         }
         onCloseAction();
     };
+    var HTMLUpdateClipboard=VPL_Util.gen_icon('copy', 'sw')+' ' + str('copy');
+    var HTMLPaste=VPL_Util.gen_icon('paste', 'sw') + ' ' + str('paste');
+    clipboard = new VPL_Clipboard('vpl_dialog_terminal_clipboard',
+                                    HTMLUpdateClipboard, function(){updateClipboard(); document.execCommand('copy');},
+                                    HTMLPaste, pasteClipboard);
 
     tdialog.dialog({
-        title : str('console'),
         closeOnEscape : false,
         autoOpen : false,
         width : 'auto',
@@ -123,7 +206,13 @@ VPL_Terminal = function(dialog_id, terminal_id, str) {
         },
         modal : true,
         dialogClass : 'vpl_ide vpl_vnc',
+        create: function() {
+            titleText = VPL_Util.setTitleBar(tdialog,'console','console',
+                    ['clipboard','keyboard'],
+                    [openClipboard,function(){terminal.focus();}]);
+        },
         close : function() {
+            clipboard.hide();
             self.disconnect();
         }
     });
@@ -145,25 +234,171 @@ VPL_VNC_Client = function(vnc_dialog_id, str) {
     var canvas = $JQVPL('#' + vnc_dialog_id + " canvas");
     var onCloseAction = function() {
     };
-    var firstStart = true;
-    var widthDiff = 0;
-    var heightDiff = 0;
+    var clipboard;
+    var needResize = true;
+    var titleText;
+    var inputarea = window.document.createElement('input');
+    inputarea.style.position = 'absolute';
+    inputarea.style.left = '0px';
+    inputarea.style.top = '-10000px';
+    inputarea.style.width = '1em';
+    inputarea.style.height = '1ex';
+    inputarea.style.opacity = '0';
+    inputarea.style.backgroundColor = 'transparent';
+    inputarea.style.borderStyle = 'none';
+    inputarea.style.outlineStyle = 'none';
+    inputarea.autocapitalize = 'off';
+    inputarea.autocomplete = 'off';
+    inputarea.autocorrect = 'off';
+    inputarea.wrap = 'off';
+    inputarea.spellcheck = 'false';
+    VNCDialog.append(inputarea);
+    var resetValue = "_________________________________________________________";
+    var lastValue = resetValue;
+    function readInput(){
+        var value = inputarea.value;
+        if(value == lastValue && value == resetValue) {
+            return;
+        }
+        var l = Math.min(value.length,lastValue.length);
+        var mod = 0;
+        for ( mod = 0; mod < l ; mod++){
+            if (value.charAt(i) != lastValue.charAt(i)){
+                break;
+            } 
+        }
+        for (var i = lastValue.length - 1; i >= value.length; i--) {
+            self.sendBackspace();
+        }
+        if (mod < value.length) {
+            self.send(value.substr(mod));
+        }
+        lastValue = value;
+        if(value.length > 500 || value.length == 0) {
+            inputarea.blur();
+            setTimeout(function() {
+                inputarea.focus();
+                inputarea.value = resetValue;
+                lastValue = resetValue;
+                try { inputarea.setSelectionRange(resetValue.length, resetValue.length); }
+                catch (err) {}
+            }, 10);
+        }
+    }
+    if (VPL_Util.isAndroid() && VPL_Util.isFirefox()) {
+        this.send = function(v) {
+            for (var i = 0; i < v.length; i++) {
+                rfb.sendKey(v.charCodeAt(i));
+            }
+        };
+        this.sendBackspace = function() {
+            rfb.sendKey(0xff08);
+        };
 
+        inputarea.value = resetValue;
+        inputarea.focus();
+        try {
+            inputarea.setSelectionRange(resetValue.length, resetValue.length);
+        } catch (e) {
+        }
+        $JQVPL(inputarea).on('change', function() {
+            readInput();
+            self.send('\r');
+        });
+        $JQVPL(inputarea).on('input', function(e) {
+            readInput();
+        });
+        $JQVPL(inputarea).on('keypress', function(e) {
+            e.stopImmediatePropagation();
+        });
+        $JQVPL(inputarea).on('focus', function() {
+            inputarea.value = resetValue;
+            lastValue = resetValue;
+            try {
+                inputarea.setSelectionRange(resetValue.length, resetValue.length);
+            } catch (e) {
+            }
+        });
+    }
+    
+    function keyboardButton(){
+        if($JQVPL(inputarea).is(':focus')){
+            inputarea.blur();
+        }else{
+            inputarea.focus();
+        }
+    }
+    function pasteClipboard() {
+        if ( self.isConnected()) {
+            rfb.clipboardPasteFrom(clipboard.getEntry2());
+        }
+    };
+    function receiveClipboard(rfb,text) {
+        clipboard.setEntry1(text);
+    };
+    function openClipboard() {
+        clipboard.show();
+    };
+    function getFocus() {
+        if(self.isConnected()){
+            rfb.get_keyboard().set_focused(true);
+        }
+    };
+    function lostFocus() {
+        if(self.isConnected()){
+            rfb.get_keyboard().set_focused(false);
+        }
+    };
+    function copyAction() {
+        clipboard.setEntry1(clipboard.getEntry1());
+        document.execCommand('copy');
+    };
+    var HTMLUpdateClipboard=VPL_Util.gen_icon('copy', 'sw')+' ' + str('copy');
+    var HTMLPaste=VPL_Util.gen_icon('paste', 'sw') + ' ' + str('paste');
+    clipboard = new VPL_Clipboard('vpl_dialog_vnc_clipboard',
+                                    HTMLUpdateClipboard, copyAction,
+                                    HTMLPaste, pasteClipboard,lostFocus);
+    canvas.on('click', function(e){
+        if(e.target == canvas[0]) {
+            getFocus();
+        } else {
+            lostFocus();
+        }
+    });
+    function displayResize(){ // TODO hot screen resize
+        if(self.isConnected()){
+            var w = VNCDialog.width();
+            var h = VNCDialog.height();
+            self.setCanvasSize(w, h);
+            rfb.get_display().viewportChange(0, 0, w, h);
+        }
+    }
     VNCDialog.dialog({
-        title : str('console'),
         closeOnEscape : false,
         autoOpen : false,
         modal : true,
         width : 'auto',
         height : 'auto',
         dialogClass : 'vpl_ide vpl_vnc',
+        create: function() {
+            titleText = VPL_Util.setTitleBar(VNCDialog,'vnc','graphic',
+                    ['clipboard','keyboard'],
+                    [openClipboard,keyboardButton]);
+        },
+        focus: getFocus,
         beforeClose : function() {
-            var w = VNCDialog.width() - widthDiff;
-            var h = VNCDialog.height() - heightDiff;
-            self.setCanvasSize(w, h);
+            if(needResize){
+                var w = VNCDialog.width();
+                var h = VNCDialog.height();
+                needResize =false;
+                self.setCanvasSize(w, h);
+            }
         },
         close : function() {
             self.disconnect();
+        },
+        resizeStop : function() {
+            needResize = true;
         }
     });
     this.updateTitle = function() {
@@ -171,7 +406,7 @@ VPL_VNC_Client = function(vnc_dialog_id, str) {
         if (message != '') {
             text += ' (' + message + ')';
         }
-        VNCDialog.dialog("option", "title", str('console') + ": " + text);
+        titleText.text(str('console') + ": " + text);
     };
     this.setTitle = function(t) {
         title = t;
@@ -203,10 +438,8 @@ VPL_VNC_Client = function(vnc_dialog_id, str) {
     }
 
     this.connect = function(secure, host, port, password, path, onClose) {
+        clipboard.setEntry1('');
         onCloseAction = onClose;
-        if (firstStart) {
-            self.setCanvasSize($JQVPL(window).width() - 150, $JQVPL(window).height() - 150);
-        }
         self.show();
         var target = $JQVPL('#' + vnc_dialog_id + " canvas")[0];
         if (!rfb) {
@@ -219,14 +452,10 @@ VPL_VNC_Client = function(vnc_dialog_id, str) {
                 'shared' : false,
                 'view_only' : false,
                 'onUpdateState' : updateState,
-                'onPasswordRequired' : null
+                'onPasswordRequired' : null,
+                'onClipboard' : receiveClipboard
             });
-            //Clipboard
-            //Cambio de tamaño
-            //Teclado en tabletas
-            //rfb.clipboardPasteFrom
-            //rfb.get_display
-            //display
+             rfb.set_local_cursor(rfb.get_display().get_cursor_uri());
         }
         if (!port) {
             port = secure ? 443 : 80;
@@ -244,17 +473,15 @@ VPL_VNC_Client = function(vnc_dialog_id, str) {
             rfb.disconnect();
         }
         onCloseAction();
+        clipboard.hide();
     };
     function round(v) {
         if (v < 100) {
             v = 100;
         }
-        return Math.floor(v / 20) * 20;
+        return Math.floor(v / 2) * 2;
     }
     this.getCanvasSize = function() {
-        if (firstStart) {
-            return ($JQVPL(window).width() - 150) + 'x' + ($JQVPL(window).height() - 150);
-        }
         return canvas.width() + "x" + canvas.height();
     };
 
@@ -262,17 +489,10 @@ VPL_VNC_Client = function(vnc_dialog_id, str) {
         canvas.width(round(w));
         canvas.height(round(h));
     };
-    function getFirstDiff() {
-        if (firstStart) {
-            firstStart = false;
-            widthDiff = VNCDialog.width() - canvas.width();
-            heightDiff = VNCDialog.height() - canvas.height();
-        }
-    }
     this.show = function() {
         VNCDialog.dialog('open');
         VNCDialog.width('auto');
         VNCDialog.height('auto');
-        VPL_Util.delay(getFirstDiff);
     };
+    self.setCanvasSize($JQVPL(window).width() - 150, $JQVPL(window).height() - 150);
 };
