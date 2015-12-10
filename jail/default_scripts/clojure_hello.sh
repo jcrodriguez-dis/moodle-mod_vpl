b18ffff02e96194a480261c7018904ecafd7d2b9
Juan Carlos Rodriguez-del-Pino <jcrodriguez@dis.ulpgc.es>
2015-12-10 12:09:44 +0000

    Script to run hello

---
+++ jail/default_scripts/clojure_hello.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This file is part of VPL for Moodle
+# Clojure language Hello source code
+# Copyright 2015 Juan Carlos Rodríguez-del-Pino
+# License http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+# Author Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
+
+cat >vpl_hello.clj <<END_OF_FILE
+(println "Hello from the Clojure language!")
+END_OF_FILE
+export VPL_SUBFILE0=vpl_hello.clj
---
+++ jail/default_scripts/sql_hello.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# This file is part of VPL for Moodle
+# SQL language Hello source code
+# Copyright 2015 Juan Carlos Rodríguez-del-Pino
+# License http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+# Author Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
+
+cat >vpl_hello.sql <<END_OF_FILE
+SELECT 'Hello from the SQL!';
+.exit
+
+END_OF_FILE
+export VPL_SUBFILE0=vpl_hello.sql
