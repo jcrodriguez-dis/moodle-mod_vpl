4b81b86bcac8a3d804d76536112ce1f2b2a0b0a5
Juan Carlos Rodriguez-del-Pino <jcrodriguez@dis.ulpgc.es>
2015-12-14 18:29:26 +0000

    Fix script error, replace $1 by "$1"

--- jail/default_scripts/php_run.sh
+++ jail/default_scripts/php_run.sh
@@ -8,7 +8,7 @@
 #load common script and check programs
 . common_script.sh
 check_program php5
-if [ $1 == "version" ] ; then
+if [ "$1" == "version" ] ; then
 	echo "#!/bin/bash" > vpl_execution
 	echo "php -v" >> vpl_execution
 	chmod +x vpl_execution
