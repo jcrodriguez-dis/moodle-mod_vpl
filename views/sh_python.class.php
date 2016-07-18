1c0accb534ee0d02ad0d89fd6de0e06ff9b2e3d0
Juan Carlos Rodriguez-del-Pino <jcrodriguez@dis.ulpgc.es>
2016-07-18 16:47:44 +0100

    Coding style correction

--- views/sh_python.class.php
+++ views/sh_python.class.php
@@ -30,11 +30,11 @@ class vpl_sh_python extends vpl_sh_text {
         if (array_key_exists( $rest, $this->reserved )) {
             $this->initTag( self::C_RESERVED );
             parent::show_pending( $rest );
-            echo self::endTag;
+            echo self::ENDTAG;
         } else if (strlen( $rest ) > 0 && $rest [0] == '_') {
             $this->initTag( self::C_VARIABLE );
             parent::show_pending( $rest );
-            echo self::endTag;
+            echo self::ENDTAG;
         } else {
             parent::show_pending( $rest );
         }
