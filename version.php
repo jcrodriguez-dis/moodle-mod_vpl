4e88400ef0925c9f207343103361caada0272551
Juan Carlos Rodríguez <jcrodriguez@dis.ulpgc.es>
2016-12-11 16:50:13 +0000

    Update version to 3.2.2

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 
 defined('MOODLE_INTERNAL') || die();
-$plugin->version = 2016120812;
+$plugin->version = 2016121112;
 $plugin->cron    = 300; // Cron check this plugin every 5 minutes.
 $plugin->requires = 2014051200; // Moodle 2.7!
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.2.1';
+$plugin->release = '3.2.2';
 $plugin->component = 'mod_vpl';
