ef7e199ab77bcbe813c412779e5d1c00a6a736da
Juan Carlos Rodríguez <jcrodriguez@dis.ulpgc.es>
2017-01-03 10:56:12 +0000

    Adds to the plugin a setting to select the editor theme

--- settings.php
+++ settings.php
@@ -116,3 +116,47 @@ $default = vpl_get_array_key( $list, 60 );
 $settings->add(
         new admin_setting_configselect( $prefix . 'discard_submission_period', get_string( 'discard_submission_period', VPL ),
                 get_string( 'discard_submission_period_description', VPL ), $default, $list ) );
+$list = array(
+        'ambiance',
+        'chaos',
+        'chrome',
+        'clouds_midnight',
+        'clouds',
+        'cobalt',
+        'crmson_editor',
+        'dawn',
+        'dreamweaver',
+        'eclipse',
+        'github',
+        'idle_fingers',
+        'iplastic',
+        'katzenmilch',
+        'kr_theme',
+        'kr',
+        'kuroir',
+        'merbivore_soft',
+        'merbivore',
+        'mono_industrial',
+        'monokai',
+        'pastel_on_dark',
+        'solarized_dark',
+        'solarized_light',
+        'sqlserver',
+        'terminal',
+        'texmate',
+        'tomorrow_night_blue',
+        'tomorrow_night_bright',
+        'tomorrow_night_eighties',
+        'tomorrow_night',
+        'tomorrow',
+        'twilight',
+        'vibrant_ink',
+        'xcode'
+);
+$themelist = array();
+foreach ( $list as $theme) {
+    $themelist [$theme] = $theme;
+}
+$settings->add(
+        new admin_setting_configselect( $prefix . 'editor_theme', get_string( 'editortheme', VPL ),
+                get_string( 'editortheme', VPL ), 'chrome', $themelist ) );
