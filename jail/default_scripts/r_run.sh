ebb6d051df8c37737382066e8db7c07cf78d2010
Juan Carlos Rodríguez <jcrodriguez@dis.ulpgc.es>
2017-03-20 23:33:18 +0000

    Fixes default R script

--- jail/default_scripts/r_run.sh
+++ jail/default_scripts/r_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This file is part of VPL for Moodle - http://vpl.dis.ulpgc.es/
 # Script for running R language
-# Copyright (C) 2012 Juan Carlos Rodríguez-del-Pino
+# Copyright (C) 2017 Juan Carlos Rodríguez-del-Pino
 # License http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
 # Author Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
 
@@ -23,9 +23,10 @@ do
 done
 #compile
 cat common_script.sh > vpl_wexecution
+cat  $SOURCE_FILE >> .Rprofile
 if [ "$1" == "batch" ] ; then
-	echo "xterm -e R --vanilla -q -f $SOURCE_FILE" >>vpl_wexecution
+	echo "xterm -e R --vanilla -f $SOURCE_FILE" >>vpl_wexecution
 else
-	echo "xterm -e R --vanilla --interactive -q -f $SOURCE_FILE" >>vpl_wexecution
+	echo "xterm -e R -q" >>vpl_wexecution
 fi
 chmod +x vpl_wexecution
