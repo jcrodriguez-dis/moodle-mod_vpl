6f209eb696a071052ba25b7166de1deada0e1967
Juan Carlos Rodríguez <jcrodriguez@dis.ulpgc.es>
2017-04-13 20:10:19 +0100

    Changes (still not functional)

--- tests/vpl_evaluate/test_vpl_evaluate.sh
+++ tests/vpl_evaluate/test_vpl_evaluate.sh
@@ -4,12 +4,13 @@ function initTest {
 	cp $ORIGINDIR/vpl_evaluate.cpp $TESTDIR
 	cp $ORIGINDIR/default_evaluate.sh $TESTDIR
 	cp $ORIGINDIR/commond_script.sh $TESTDIR
-	cp $CASESDIR/vpl_run$1.sh $TESTDIR/vpl_run.sh
-	cp $CASESDIR/vpl_evaluate$1.cases $TESTDIR/vpl_evaluate.cases
-	cp $CASESDIR/vpl_test_evaluate$1.sh $TESTDIR/vpl_test_evaluate.sh
+	cp $CASESDIR/vpl_run_$1.sh $TESTDIR/vpl_run.sh
+	cp $CASESDIR/vpl_evaluate_$1.cases $TESTDIR/vpl_evaluate.cases
+	cp $CASESDIR/vpl_test_evaluate_$1.sh $TESTDIR/vpl_test_evaluate.sh
 }
 
 function runTest {
+	echo "Test: $1"
 	cd $TESTDIR
 	chmod +x *.sh
 	./default_evaluate.sh
@@ -24,11 +25,15 @@ function runTest {
 }
 
 function evalTest {
-	
+	cd $TESTDIR
+	./vpl_test_evaluate
+	local RET=$?
+    cd ..
+    return $RET
 }
 
 function runAllTests {
-	local cases="$(find $CASESDIR -name "vpl_run*.sh" -print | sed 's/^vpl_run\///g' | sed 's/.sh$\///g' )"
+	local cases="$(find $CASESDIR -name "vpl_run_*.sh" -print | sed 's/^vpl_run_\///g' | sed 's/.sh$\///g' )"
 	for case in $cases
 	do
 		initTest $case
