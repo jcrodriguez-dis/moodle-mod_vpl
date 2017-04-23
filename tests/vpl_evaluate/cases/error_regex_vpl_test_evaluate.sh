bacb810d1fc001ef8c43e3e04f4b5b1289e1497f
Juan Carlos Rodríguez del Pino <jcrodriguez@dis.ulpgc.es>
2017-04-23 22:46:02 +0100

    Code refactored

--- tests/vpl_evaluate/cases/error_regex_vpl_test_evaluate.sh
+++ tests/vpl_evaluate/cases/error_regex_vpl_test_evaluate.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 if [ -s "$VPLTESTERRORS" ] ; then
-    echo "The program has generated the following errors"
-    cat $VPLTESTERRORS
     exit 1
 fi
 grep -e 'Grade :=>> 6$' "$VPLTESTOUTPUT" >/dev/null
-if [ $? != 0 ] ; then
-    echo "Test: $1 failed"
-    cat "$VPLTESTOUTPUT"
-    exit 1
-fi
-exit 0
