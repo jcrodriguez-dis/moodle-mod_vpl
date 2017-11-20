36b2c03d8ae44beb256c3e5063c4d32c4c6ce2b0
Juan Carlos Rodríguez <jcrodriguez@dis.ulpgc.es>
2017-11-20 16:54:01 +0000

    Updates version number and release type

--- version.php
+++ version.php
@@ -26,10 +26,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2017100112;
+$plugin->version = 2017110112;
 $plugin->cron    = 300; // Cron check this plugin every 5 minutes.
 $plugin->requires = 2014051200; // Moodle 2.7!
-$plugin->maturity = MATURITY_BETA;
-$plugin->release = '3.3dev';
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '3.3';
 
 $plugin->component = 'mod_vpl';
