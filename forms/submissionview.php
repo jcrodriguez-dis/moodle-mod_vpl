24600d999c308fa9f6d651cc60d091f23c21af18
Juan Carlos Rodríguez <jcrodriguez@dis.ulpgc.es>
2017-11-20 16:49:29 +0000

    Refactorization

--- forms/submissionview.php
+++ forms/submissionview.php
@@ -32,94 +32,91 @@ require_once(dirname(__FILE__).'/../views/sh_factory.class.php');
 
 global $CFG, $USER;
 
-try {
-    require_login();
-    $id = required_param( 'id', PARAM_INT );
-    $userid = optional_param( 'userid', false, PARAM_INT );
-    $vpl = new mod_vpl( $id );
-    if ($userid) {
-        $vpl->prepare_page( 'forms/submissionview.php', array (
-                'id' => $id,
-                'userid' => $userid
-        ) );
-    } else {
-        $vpl->prepare_page( 'forms/submissionview.php', array (
-                'id' => $id
-        ) );
-    }
-    if (! $vpl->is_visible()) {
-        \mod_vpl\event\vpl_security::log( $vpl );
-        vpl_redirect( '?id=' . $id, get_string( 'notavailable' ) );
-    }
-    $course = $vpl->get_course();
-    $instance = $vpl->get_instance();
-
-    $submissionid = optional_param( 'submissionid', false, PARAM_INT );
-    // Read records.
-    if ($userid && $userid != $USER->id) {
-        // Grader.
-        $vpl->require_capability( VPL_GRADE_CAPABILITY );
-        $grader = true;
-        if ($submissionid) {
-            $subinstance = $DB->get_record( 'vpl_submissions', array (
-                    'id' => $submissionid
-            ) );
-        } else {
-            $subinstance = $vpl->last_user_submission( $userid );
-        }
-    } else {
-        // View own submission.
-        $vpl->require_capability( VPL_VIEW_CAPABILITY );
-        $userid = $USER->id;
-        $grader = false;
-        if ($submissionid && $vpl->has_capability( VPL_GRADE_CAPABILITY )) {
-            $subinstance = $DB->get_record( 'vpl_submissions', array (
-                    'id' => $submissionid
-            ) );
-        } else {
-            $subinstance = $vpl->last_user_submission( $userid );
-        }
-    }
-    if ($subinstance != null && $subinstance->vpl != $vpl->get_instance()->id) {
-        print_error( 'invalidcourseid' );
-    }
-    if ($USER->id == $userid) {
-        $vpl->network_check();
-        $vpl->password_check();
-    }
-    // Print header.
-    vpl_sh_factory::include_js();
-
-    $vpl->print_header( get_string( 'submissionview', VPL ) );
-    $vpl->print_view_tabs( basename( __FILE__ ) );
-    // Display submission.
-
-
-    // Check consistence.
-    if (! $subinstance) {
-        notice( get_string( 'nosubmission', VPL ), vpl_mod_href( 'view.php', 'id', $id, 'userid', $userid ) );
-    }
-    $submissionid = $subinstance->id;
-
-    if ($vpl->is_inconsistent_user( $subinstance->userid, $userid )) {
-        print_error( 'vpl submission user inconsistence' );
-    }
-    if ($vpl->get_instance()->id != $subinstance->vpl) {
-        print_error( 'vpl submission vpl inconsistence' );
-    }
-    $submission = new mod_vpl_submission( $vpl, $subinstance );
-
-    if ($vpl->get_visiblegrade() || $vpl->has_capability( VPL_GRADE_CAPABILITY )) {
-        if ($submission->is_graded()) {
-            echo '<h2>' . get_string( 'grade' ) . '</h2>';
-            $submission->print_grade( true );
-        }
-    }
-    $vpl->print_variation( $subinstance->userid );
-    $submission->print_submission();
-    $vpl->print_footer();
-    \mod_vpl\event\submission_viewed::log( $submission );
-    vpl_sh_factory::syntaxhighlight();
-} catch ( Exception $e ) {
-    print_error( $e->getMessage() );
+require_login();
+$id = required_param( 'id', PARAM_INT );
+$userid = optional_param( 'userid', false, PARAM_INT );
+$vpl = new mod_vpl( $id );
+if ($userid) {
+    $vpl->prepare_page( 'forms/submissionview.php', array (
+            'id' => $id,
+            'userid' => $userid
+    ) );
+} else {
+    $vpl->prepare_page( 'forms/submissionview.php', array (
+            'id' => $id
+    ) );
 }
+if (! $vpl->is_visible()) {
+    \mod_vpl\event\vpl_security::log( $vpl );
+    vpl_redirect( '?id=' . $id, get_string( 'notavailable' ) );
+}
+$course = $vpl->get_course();
+$instance = $vpl->get_instance();
+
+$submissionid = optional_param( 'submissionid', false, PARAM_INT );
+// Read records.
+if ($userid && $userid != $USER->id) {
+    // Grader.
+    $vpl->require_capability( VPL_GRADE_CAPABILITY );
+    $grader = true;
+    if ($submissionid) {
+        $subinstance = $DB->get_record( 'vpl_submissions', array (
+                'id' => $submissionid
+        ) );
+    } else {
+        $subinstance = $vpl->last_user_submission( $userid );
+    }
+} else {
+    // View own submission.
+    $vpl->require_capability( VPL_VIEW_CAPABILITY );
+    $userid = $USER->id;
+    $grader = false;
+    if ($submissionid && $vpl->has_capability( VPL_GRADE_CAPABILITY )) {
+        $subinstance = $DB->get_record( 'vpl_submissions', array (
+                'id' => $submissionid
+        ) );
+    } else {
+        $subinstance = $vpl->last_user_submission( $userid );
+    }
+}
+if ($subinstance != null && $subinstance->vpl != $vpl->get_instance()->id) {
+    print_error( 'invalidcourseid' );
+}
+if ($USER->id == $userid) {
+    $vpl->network_check();
+    $vpl->password_check();
+}
+// Print header.
+vpl_sh_factory::include_js();
+
+$vpl->print_header( get_string( 'submissionview', VPL ) );
+$vpl->print_view_tabs( basename( __FILE__ ) );
+// Display submission.
+
+
+// Check consistence.
+if (! $subinstance) {
+    vpl_redirect(vpl_mod_href( 'view.php', 'id', $id, 'userid', $userid ),
+                 get_string( 'nosubmission', VPL ));
+}
+$submissionid = $subinstance->id;
+
+if ($vpl->is_inconsistent_user( $subinstance->userid, $userid )) {
+    print_error( 'vpl submission user inconsistence' );
+}
+if ($vpl->get_instance()->id != $subinstance->vpl) {
+    print_error( 'vpl submission vpl inconsistence' );
+}
+$submission = new mod_vpl_submission( $vpl, $subinstance );
+
+if ($vpl->get_visiblegrade() || $vpl->has_capability( VPL_GRADE_CAPABILITY )) {
+    if ($submission->is_graded()) {
+        echo '<h2>' . get_string( 'grade' ) . '</h2>';
+        $submission->print_grade( true );
+    }
+}
+$vpl->print_variation( $subinstance->userid );
+$submission->print_submission();
+$vpl->print_footer();
+\mod_vpl\event\submission_viewed::log( $submission );
+vpl_sh_factory::syntaxhighlight();
