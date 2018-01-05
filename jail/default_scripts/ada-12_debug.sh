42fe2565275b4579ec6a27a014004a068267b334
Juan Carlos Rodríguez <jcrodriguez@dis.ulpgc.es>
2018-01-05 13:41:24 +0000

    New and updated run/debug scripts ready to be selected

---
+++ jail/default_scripts/ada-12_debug.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# This file is part of VPL for Moodle
+# Script for debugging ADA language
+# Copyright (C) 2012 Juan Carlos Rodríguez-del-Pino
+# License http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+# Author Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
+
+# @vpl_script_description Using GDB and GNAT with Ada 2012 features 
+# load common script and check programs and VPL environment vars
+. common_script.sh
+check_program gnat
+check_program gdb
+get_source_files adb
+
+# compile
+gnat make -gnat12 -gnatW8 -q -g -o program "$SOURCE_FILE0"
+if [ -f program ] ; then
+	cat common_script.sh > vpl_execution
+	echo "gdb program" >> vpl_execution
+	chmod +x vpl_execution
+fi
