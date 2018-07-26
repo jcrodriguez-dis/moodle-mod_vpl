9823ac0d0c200cb104eb5bc148635213f3dce854
Juan Carlos Rodríguez <jcrodriguez@dis.ulpgc.es>
2018-07-26 18:43:33 +0100

    Sets file charset when compiling with g++

--- jail/default_scripts/default_evaluate.sh
+++ jail/default_scripts/default_evaluate.sh
@@ -35,7 +35,7 @@ else
 		fi
 		mv vpl_evaluate.cpp.save vpl_evaluate.cpp
 		check_program g++
-		g++ vpl_evaluate.cpp -g -lm -lutil -o .vpl_tester
+		g++ -finput-charset=UTF-8 vpl_evaluate.cpp -g -lm -lutil -o .vpl_tester
 		if [ ! -f .vpl_tester ] ; then
 			echo "Error compiling evaluation program"
 			exit 1
