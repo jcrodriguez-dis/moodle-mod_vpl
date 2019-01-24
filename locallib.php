d6008e3a773e4a773b217b265f3dcf57cda602ba
Juan Carlos Rodríguez <jcrodriguez@dis.ulpgc.es>
2019-01-24 16:31:15 +0000

    Fixes vpl_get_set_session_var

--- locallib.php
+++ locallib.php
@@ -62,7 +62,7 @@ function vpl_get_set_session_var($varname, $default, $parname = null) {
     if (isset( $SESSION->$fullname )) { // Exists var?
         $res = $SESSION->$fullname;
     }
-    $res = optional_param( $parname, $res, PARAM_ALPHANUM );
+    $res = optional_param( $parname, $res, PARAM_RAW );
     $SESSION->$fullname = $res;
     return $res;
 }
