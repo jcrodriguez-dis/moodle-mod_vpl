779bac2ce4cc96909d4cd10b30cd296ff20b0c52
Juan Carlos Rodriguez-Del-Pino <jc.rodriguezdelpino@ulpgc.es>
2019-10-21 16:20:23 +0100

    Updates version number

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2018081117;
+$plugin->version = 2019102116;
 $plugin->cron    = 300; // Cron check this plugin every 5 minutes.
 $plugin->requires = 2014051200; // Moodle 2.7!
 $plugin->maturity = MATURITY_STABLE;
