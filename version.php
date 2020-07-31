1be257ba4809105c111cb0a0d30c2a03fdac3fd4
Juan Carlos Rodriguez-Del-Pino <jc.rodriguezdelpino@ulpgc.es>
2020-07-31 19:28:36 +0100

    Increases version number
    Removes the cron value from the plugin object.

--- version.php
+++ version.php
@@ -26,10 +26,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2020041316;
-$plugin->cron    = 300; // Cron check this plugin every 5 minutes.
+$plugin->version = 2020073018;
 $plugin->requires = 2014051200; // Moodle 2.7!
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.3.7';
+$plugin->release = '3.3.8';
 
 $plugin->component = 'mod_vpl';
