734e179ae5ca207120cc56768232f9e2879facf0
Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
2021-01-11 09:51:26 +0000

    Adds functions to manage php config parameters

---
+++ classes/util/phpconfig.php
@@ -0,0 +1,106 @@
+<?php
+// This file is part of VPL for Moodle - http://vpl.dis.ulpgc.es/
+//
+// VPL for Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// VPL for Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with VPL for Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Class to manage PHP configuration
+ *
+ * @package   mod_vpl
+ * @copyright 2020 onwards Juan Carlos Rodríguez-del-Pino
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author    Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
+ */
+namespace mod_vpl\util;
+
+defined( 'MOODLE_INTERNAL' ) || die();
+
+/**
+ * Class to manage PHP config
+ */
+class phpconfig {
+    /**
+     * Keeps conversions from Kb, Mb or Gb to bytes.
+     * 
+     * @var array Key 'k', 'm', 'g' => value
+     */
+    const byteconverter = array( 'k' => 1024,
+                                 'm' => 1024 * 1024,
+                                 'g' => 1024 * 1024 * 1024 );
+    /**
+     * Returns number of bytes from string values in Kb, Mb or Gb
+     * 
+     * @param string $value Value to convert e.g 2M, 1.5G, 32K
+     * 
+     * @return int Nummer of bytes
+     */
+    static public function get_bytes(string $value): int {
+        $value = strtolower(trim($value));
+        if (strlen($value) > 0) {
+            $unity = substr($value, -1);
+            if ($unity == 'b') {
+                $unity = substr($value, strlen($value) - 2, 1);
+            }
+            if (isset(self::byteconverter[$unity])) {
+                $bytes = substr($value, 0, -1) * self::byteconverter[$unity];
+            } else {
+                $bytes = (int) $value;
+            }
+        } else {
+            $bytes = 0;
+        }
+        return $bytes;
+    }
+
+    /**
+     * Return the post maximum size in bytes
+     * 
+     * @return int Number of bytes
+     */
+    static public function get_post_max_size(): int {
+        return self::get_bytes(ini_get('post_max_size'));
+    }
+
+    /**
+     * Increase PHP memory limit to post_max_size * 3
+     * 
+     * @return void
+     */
+    static public function increase_memory_limit(): void {
+        $bytes = self::get_post_max_size() * 3;
+        if ($bytes > self::get_bytes(ini_get('memory_limit'))) {
+            $newmemorylimit = (int) ($bytes / self::byteconverter['k']);
+            ini_set('memory_limit', $newmemorylimit . 'K');
+        }
+    }
+
+    /**
+     * Throws an exception if the PHP free memory is less than needed
+     * 
+     * @param int $memoryneeded Memory needed
+     * 
+     * @return void
+     */
+    static public function checks_free_memory(int $memoryneeded): void {
+        $memoryused = memory_get_usage();
+        $memorylimit = self::get_bytes(ini_get('memory_limit'));
+        if ($memorylimit - $memoryused < $memoryneeded) {
+            self::increase_memory_limit();
+            $memorylimit = self::get_bytes(ini_get('memory_limit'));
+            if ($memorylimit - $memoryused < $memoryneeded) {
+                throw new \Exception(get_string('outofmemory', 'vpl'));
+            }
+        }
+    }
+}
---
+++ tests/util_phpconfig_test.php
@@ -0,0 +1,67 @@
+<?php
+// This file is part of VPL for Moodle - http://vpl.dis.ulpgc.es/
+//
+// VPL for Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// VPL for Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with VPL for Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Unit tests for class \mod_vpl\util\phpconfig
+ *
+ * @package   mod_vpl
+ * @copyright Juan Carlos Rodríguez-del-Pino
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author    Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once(dirname(__FILE__).'/../locallib.php');
+
+/**
+ * Unit tests for \mod_vpl\util\phpconfig class.
+ * 
+ * @group mod_vpl
+ */
+class mod_vpl_util_phpconfig extends advanced_testcase {
+   /**
+     * Method to test function get_bytes(string $value): int
+     */
+    public function test_get_bytes() {
+        $cases = [[123, '123'], [13 * 1024, '13k'], [7 * 1024 * 1024, '7M'], [1024 * 1024 * 1024, '1G']];
+        foreach ($cases as $case) {
+            $this->assertEquals( $case[0], \mod_vpl\util\phpconfig::get_bytes($case[1]), $case[1]);
+        }
+
+    }
+   /**
+     * Method to test function get_post_max_size(): int
+     */
+    public function test_get_post_max_size() {
+        // Untestable, checks only callable
+        $this->assertTrue(\mod_vpl\util\phpconfig::get_post_max_size() > 0);
+    }
+   /**
+     * Method to test function increase_memory_limit(): void
+     */
+    public function test_increase_memory_limit() {
+        $memory_limit = \mod_vpl\util\phpconfig::get_bytes(ini_get('memory_limit'));
+        $max_post = \mod_vpl\util\phpconfig::get_post_max_size();
+        \mod_vpl\util\phpconfig::increase_memory_limit();
+        $this->assertTrue($max_post * 3 <= \mod_vpl\util\phpconfig::get_bytes(ini_get('memory_limit')));
+    }
+   /**
+     * Method to test function checks_free_memory(int $memoryneeded): void
+     */
+    public function test_checks_free_memory() {
+    }
+}
