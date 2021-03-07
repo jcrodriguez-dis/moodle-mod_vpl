c732c6d5393b8196b1dd3f5500ab2344b27fb28b
Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
2021-03-07 13:45:19 +0000

    Cleans the executionfiles_help string

--- lang/en/vpl.php
+++ lang/en/vpl.php
@@ -366,79 +366,11 @@ $string ['step'] = 'Step';
 
 $string ['check_jail_servers_help'] = "<p>This page check and show the status of execution servers used
 for this activity.</p>";
-$string ['executionfiles_help'] = '<h2>Introduction</h2>
-<p>Here you set the files that are needed to prepare the execution,
-debug or assessment of a submission. This includes scripting files,
-program test files and data files.</p>
-<h2>Default script to run or debug</h2>
-<p>If you don\'t set script files for run or  debug submissions, the system
+$string ['executionfiles_help'] = '<p>Here you set the files that are needed to prepare the execution,
+debug or assessment of a submission. This includes scripting files, program test files and data files.</p>
+<p>If you don\'t set script files for run or debug submissions, the system
 will resolve the language you use (based on file name extensions) and use a
-predefined script.
-<h2>Automatic evaluation</h2>
-<p>The incorporates features to facilitate the evaluation of student\'s submissions.
-This feature allows to run the student program and check its output for a given input.
-To set up the evaluation cases you must populate the file &quot;vpl_evaluate.cases&quot;.
-<p>The file "vpl_evaluate.cases" has the following format:
-<ul>
-<li> "<strong>case </strong>= Description of case": Optional. Set an start of test case definition.</li>
-<li> "<strong>input </strong>= text": can use several lines. Ends with other instruction.</li>
-<li> "<strong>output </strong>= text": can use several lines. Ends with other instruction. A case can have differents correct output. There are three types of output: numbers, text and exact test:
-<ul>
-<li> <strong>number</strong>: defined as sequence of numbers (integers and floats). Only numbers in the output are checked, other text are ignored. Floats are checked with tolerance</li>
-<li> <strong>text</strong>: defined as text without double quote. Only words are checked and the rest of chars are ignored, the comparation is case-insensitive </li>
-<li> <strong>exact text</strong>: defined as text into double quote. The exact match is used to test the output.</li>
-</ul>
-</li>
-<li> "<strong>grade reduction</strong> = [value|percentage%]" : By default an error reduces student\'s grade
-(starts with maxgrade) by (grade_range/number of cases) but with this instruction you can change
-the reduction value or percentage.</li>
-</ul>
-</p>
-<h2>General use</h2>
-<p>A new file can be added by writing its name in the box  &quot;<b>Add file</b>&quot;
-and then clicking on the button &quot;<b>Add file</b>&quot;.</p>
-<p>An existing file can be uploaded by means of the  &quot;<b>Upload file</b>&quot;.<p>All the added  or uploaded files can
-be edited, and all of them, except the three scripting files mentioned
-below, can be renamed or deleted.</p>
-<h2>Manual run, execute or evaluation</h2>
-<p>Three scripting files to prepare each of the actions may be set.
-These files have predefined names: <b>vpl_run.sh</b> (execution),
-<b>vpl_debug.sh</b>  (debug) and <b>vpl_evaluate.sh</b> (assessment).</p>
-<p>The execution of any of those scripting files should generate a
-file named <b>vpl_execution</b>.
-This file must be a binary executable or a script beginning with &quot;#!/bin/sh &quot;.
-The non-generation of this file impedes to run the selected action.</p>
-<p>If the activity that you are configuring is "based on" other activity,
-the files of the base activity are added automatically.
-The contents of the vpl_run.sh, vpl_debug.sh and vpl_evaluate.sh files
-are concatenated from the deepest level of "based on" to the current.</P>
-<p>Finally, the file <b>vpl_environment.sh</b> is automatically added.
-This scripting file contain information about the submission.
-The information come as environment variables: </p>
-<ul> <li> LANG:  used language. </li>
-<li> LC_ALL: same value as LANG. </li>
-<li> VPL_MAXTIME: maximum execution time in seconds. </li>
-<li> VPL_FILEBASEURL: URL to access the files of the course. </li>
-<li> VPL_SUBFILE#:  each name of the files submitted by the student. # Ranges from 0 to number of submitted files. </Li>
-<li> VPL_SUBFILES: list of all submitted files. </li>
-<li> VPL_VARIATION + id: where id is the variation order starting with 0 and the value is the value of the variation. </li>
-</ul>
-If the action requested is evaluation, then the following vars are added too.
-<ul>
-	<li>VPL_MAXTIME: max time of execution in seconds.</li>
-	<li>VPL_MAXMEMORY: max memmory usable</li>
-	<li>VPL_MAXFILESIZE: max file size in byte that can be create.</li>
-	<li>VPL_MAXPROCESSES: max number of procceses that can be run simultaneous.</li>
-	<Li>VPL_FILEBASEURL: URL to the course files.</Li>
-	<li>VPL_GRADEMIN: Min grade for this activity</li>
-	<li>VPL_GRADEMAX: Max grade for this activity</li>
-</ul>
-<h2>Assessment result</h2>
-<p>Evaluation output is processed to extract, if possible, comments and a proposed grade for the assessment.
-Comments can be set in two ways: with a line comment defined by a line beginning with \'Comment :=&gt;&gt;\' or
-with block comments starting with a line containing only \'&lt;|--\' and ending with a line containing only \'--|&gt;\'.
-The grade is taken from the last line that begins with \'Grade :=&gt;&gt;\'.
-</p>';
+predefined script.';
 $string ['executionoptions_help'] = '<p>Various execution options are set in this page</p>
 <ul>
 <li><b>Based on</b>: sets other VPL instance from which some features are imported:
