f83e59bfd8ad8d23e428c82ec7bca2cc23a92d7a
Juan Carlos Rodríguez del Pino <jc.rodriguezdelpino@ulpgc.es>
2021-03-27 19:26:24 +0000

    Adds new acceptance tests

---
+++ tests/behat/behat_mod_vpl.php
@@ -0,0 +1,62 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Steps definitions for VPL activity.
+ *
+ * @package   mod_vpl
+ * @category  test
+ * @copyright 2021 Juan Carlos Rodríguez-del-Pino
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// NOTE: no MOODLE_INTERNAL test here, this file may be required by behat before including /config.php.
+
+require_once(__DIR__ . '/../../../../lib/behat/behat_base.php');
+
+/**
+ * VPL activity definitions.
+ *
+ * @package   mod_vpl
+ * @category  test
+ * @copyright 2021 Juan Carlos Rodríguez-del-Pino
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class behat_mod_vpl extends behat_base {
+
+    /**
+     * Click on any element
+     *
+     * @Given /^I click on "([^"]*)" in VPL$/
+     * @param string $selector
+     * @return void
+     */
+    public function i_click_on_selector_in_vpl($selector) {
+        $script = "$(\"$selector\").click();";
+        $this->getSession()->evaluateScript($script);
+    }
+
+    /**
+     * Accept confirm popup
+     *
+     * @Given /^I accept confirm in VPL$/
+     * @return void
+     */
+    public function i_accept_confirm_in_vpl() {
+        $script = "window.confirm = function(){return true;};";
+        $this->getSession()->evaluateScript($script);
+    }
+}
---
+++ tests/behat/test_cases.feature
@@ -0,0 +1,40 @@
+@mod @mod_vpl
+Feature: In an VPL activity, editing teacher change test cases
+  In order to change test cases
+  As an editing teacher
+  I access to a VPL activity variation page and create, modify, and delete varitions
+
+  Background:
+    Given the following "courses" exist:
+      | fullname | shortname | category | groupmode |
+      | Course 1 | C1 | 0 | 1 |
+    And the following "users" exist:
+      | username | firstname | lastname | email |
+      | teacher1 | Teacher | 1 | teacher1@example.com |
+      | student1 | Student | 1 | student1@example.com |
+    And the following "course enrolments" exist:
+      | user | course | role |
+      | teacher1 | C1 | editingteacher |
+      | student1 | C1 | student |
+    And I log in as "teacher1"
+    And I am on "Course 1" course homepage with editing mode on
+    And I add a "Virtual programming lab" to section "1" and I fill the form with:
+      | id_name | VPL activity name |
+      | id_shortdescription | VPL activity short description |
+      | id_duedate_enabled | "" |
+      | id_maxfiles | 33 |
+      | id_grade_modgrade_type | None |
+    And I log out
+
+  @javascript
+  Scenario: A teacher access test cases
+    Given I log in as "teacher1"
+    And I am on "Course 1" course homepage
+    And I follow "VPL activity name"
+    When I navigate to "Test cases" in current page administration
+    Then I should see "vpl_evaluate.cases"
+    
+    
+
+
+
