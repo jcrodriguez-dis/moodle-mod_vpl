7a28c28fa69f9d591e4dba1f8eee9ed5963bccb1
Juan Carlos Rodríguez-del-Pino <jc.rodriguezdelpino@ulpgc.es>
2022-07-05 13:35:53 +0100

    Updates to run web applications using a local browser
    Reduces server resources needed drastically.
    Requires jail server V2.8 or later.

--- jail/default_scripts/html_run.sh
+++ jail/default_scripts/html_run.sh
@@ -1,29 +1,73 @@
 #!/bin/bash
 # This file is part of VPL for Moodle - http://vpl.dis.ulpgc.es/
-# Script for running HTML language
-# Copyright (C) 2014 Juan Carlos Rodríguez-del-Pino
+# Script for running HTML using the PHP Built-in web server
+# Copyright (C) 2022 onwards Juan Carlos Rodríguez-del-Pino
 # License http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
 # Author Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
 
-# @vpl_script_description Using "firefox" with the first file
+# @vpl_script_description Using "php -n -f" access index.html
+
 # load common script and check programs
 . common_script.sh
-check_program x-www-browser firefox
+
+check_program php php5
+PHP=$PROGRAM
 if [ "$1" == "version" ] ; then
 	echo "#!/bin/bash" > vpl_execution
-	echo "$PROGRAM --version | head -n3" >> vpl_execution
+	echo "$PHP -v" >> vpl_execution
 	chmod +x vpl_execution
 	exit
 fi
 
 compile_typescript
 compile_scss
+PHPCONFIGFILE=$($PHP -i 2>/dev/null | grep "Loaded Configuration File" | sed 's/^[^\/]*//' )
+if [ "$PHPCONFIGFILE" == "" ] ; then
+	touch .php.ini
+else
+	cp $PHPCONFIGFILE .php.ini	
+fi
+#Configure session
+SESSIONPATH=$HOME/.php_sessions
+mkdir $SESSIONPATH
+#Generate php.ini
+cat >> .php.ini <<END_OF_INI
+	
+session.save_path="$SESSIONPATH"
+error_reporting=E_ALL
+display_errors=On
+display_startup_errors=On
+END_OF_INI
 
-check_program x-www-browser firefox
-get_first_source_file html htm
-# Generate file with source files
-cat > vpl_wexecution <<END_OF_SCRIPT
+#Generate router
+cat >> .router.php << 'END_OF_PHP'
+<?php $path=urldecode(parse_url($_SERVER["REQUEST_URI"],PHP_URL_PATH));
+$file='.'.$path;
+if(is_file($file) || is_file($file.'/index.php') || is_file($file.'/index.html') ){
+    unset($path,$file);
+    return false;
+}
+$pclean=htmlentities($path);
+http_response_code(404);
+header(':', true, 404);
+?>
+<!doctype html>
+<html><head><title>404 Not found</title>
+<style>h1{background-color: aqua;text-align:center} code{font-size:150%}</style>
+</head>
+<body><h1>404 Not found</h1><p>The requested resource <code><?php echo "'$pclean'"; ?></code> 
+was not found on this server</body></html>
+END_OF_PHP
+# Calculate IP 127.X.X.X: (random port)
+if [ "$UID" == "" ] ; then
+	echo "Error: UID not set"
+fi
+export serverPort=$((10000+$RANDOM%50000))
+export serverIP="127.$((1+$UID/1024%64)).$((1+$UID/16%64)).$((10+$UID%16))"
+echo "$serverIP:$serverPort" > .vpl_localserveraddress
+cat common_script.sh > vpl_webexecution
+cat >> vpl_webexecution <<END_OF_SCRIPT
 #!/bin/bash
-$PROGRAM "$FIRST_SOURCE_FILE"
+$PHP -c .php.ini -S "$serverIP:$serverPort" .router.php
 END_OF_SCRIPT
-chmod +x vpl_wexecution
+chmod +x vpl_webexecution
--- jail/default_scripts/php_run.sh
+++ jail/default_scripts/php_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This file is part of VPL for Moodle - http://vpl.dis.ulpgc.es/
-# Script for running PHP language
+# Script for running PHP language using the PHP Built-in web server
 # Copyright (C) 2012 onwards Juan Carlos Rodríguez-del-Pino
 # License http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
 # Author Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
@@ -19,30 +19,27 @@ if [ "$1" == "version" ] ; then
 	exit
 fi
 
-get_source_files php
+get_source_files php NOERROR
+SIFS=$IFS
 IFS=$'\n'
 for file_name in $SOURCE_FILES
 do
 	php -l "$file_name" > /dev/null
 done
 IFS=$SIFS
-
-if [ -f index.php ] ; then
-	check_program x-www-browser firefox
-	BROWSER=$PROGRAM
-	compile_typescript
-	compile_scss
-	PHPCONFIGFILE=$($PHP -i 2>/dev/null | grep "Loaded Configuration File" | sed 's/^[^\/]*//' )
-	if [ "$PHPCONFIGFILE" == "" ] ; then
-		touch .php.ini
-	else
-		cp $PHPCONFIGFILE .php.ini	
-	fi
-    #Configure session
-	SESSIONPATH=$HOME/.php_sessions
-	mkdir $SESSIONPATH
-	#Generate php.ini
-	cat >> .php.ini <<END_OF_INI
+compile_typescript
+compile_scss
+PHPCONFIGFILE=$($PHP -i 2>/dev/null | grep "Loaded Configuration File" | sed 's/^[^\/]*//' )
+if [ "$PHPCONFIGFILE" == "" ] ; then
+	touch .php.ini
+else
+	cp $PHPCONFIGFILE .php.ini	
+fi
+#Configure session
+SESSIONPATH=$HOME/.php_sessions
+mkdir $SESSIONPATH
+#Generate php.ini
+cat >> .php.ini <<END_OF_INI
 	
 session.save_path="$SESSIONPATH"
 error_reporting=E_ALL
@@ -50,8 +47,8 @@ display_errors=On
 display_startup_errors=On
 END_OF_INI
 
-    #Generate router
-    cat >> .router.php << 'END_OF_PHP'
+#Generate router
+cat >> .router.php << 'END_OF_PHP'
 <?php $path=urldecode(parse_url($_SERVER["REQUEST_URI"],PHP_URL_PATH));
 $file='.'.$path;
 if(is_file($file) || is_file($file.'/index.php') || is_file($file.'/index.html') ){
@@ -69,22 +66,16 @@ header(':', true, 404);
 <body><h1>404 Not found</h1><p>The requested resource <code><?php echo "'$pclean'"; ?></code> 
 was not found on this server</body></html>
 END_OF_PHP
-	while true; do
-	   	PHPPORT=$((6000+$RANDOM%25000))
-	   	netstat -tln | grep -q ":$PHPPORT "
-	   	[ "$?" != "0" ] && break
-	done
-	cat common_script.sh > vpl_wexecution
-	cat >> vpl_wexecution <<END_OF_SCRIPT
-#!/bin/bash
-$PHP -c .php.ini -S "127.0.0.1:$PHPPORT" .router.php &
-$BROWSER "127.0.0.1:$PHPPORT"
-wait_end $BROWSER
-END_OF_SCRIPT
-    chmod +x vpl_wexecution
-else
-	get_first_source_file php
-    cat common_script.sh > vpl_execution
-    echo "php "\"$FIRST_SOURCE_FILE\"" \$@" >>vpl_execution
-    chmod +x vpl_execution
+# Calculate IP 127.X.X.X: (random port)
+if [ "$UID" == "" ] ; then
+	echo "Error: UID not set"
 fi
+export serverPort=$((10000+$RANDOM%50000))
+export serverIP="127.$((1+$UID/1024%64)).$((1+$UID/16%64)).$((10+$UID%16))"
+echo "$serverIP:$serverPort" > .vpl_localserveraddress
+cat common_script.sh > vpl_webexecution
+cat >> vpl_webexecution <<END_OF_SCRIPT
+#!/bin/bash
+$PHP -c .php.ini -S "$serverIP:$serverPort" .router.php
+END_OF_SCRIPT
+chmod +x vpl_webexecution
