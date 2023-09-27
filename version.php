4e51da4d09c7bfe1e4a2ed5bf336cea8620344bb
Juan Carlos Rodríguez del Pino <jc.rodriguezdelpino@ulpgc.es>
2023-09-27 16:01:26 +0100

    Updates version number and date

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023082512;
+$plugin->version = 2023092512;
 $plugin->requires = 2018051713; // Moodle 3.5!
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '4.2.0';
+$plugin->release = '4.2.1';
 
 $plugin->component = 'mod_vpl';
