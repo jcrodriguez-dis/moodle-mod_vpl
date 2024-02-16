859e92bb019f75716c7079ac938a8289d9c82143
Astor Bizard <13221286+Astor-Bizard@users.noreply.github.com>
2024-02-16 14:22:52 +0100

    Allow whitespaces before and after <|-- and --|>

--- vpl_submission.class.php
+++ vpl_submission.class.php
@@ -936,11 +936,11 @@ class mod_vpl_submission {
     public static function find_proposedcomment(&$text) {
         $usecrnl = vpl_detect_newline($text) == "\r\n";
         if ($usecrnl) {
-            $startcommentreg = '/^(Comment :=>>([^\\r\\n]*)|<\\|--)\\r?$/m';
-            $endcommentreg = '/^--\\|>\\r?$/m';
+            $startcommentreg = '/^(Comment :=>>([^\\r\\n]*)|\\s*<\\|--\\s*)\\r?$/m';
+            $endcommentreg = '/^\\s*--\\|>\\s*\\r?$/m';
         } else {
-            $startcommentreg = '/^(Comment :=>>(.*)|<\\|--)$/m';
-            $endcommentreg = '/^--\\|>$/m';
+            $startcommentreg = '/^(Comment :=>>(.*)|\\s*<\\|--\\s*)$/m';
+            $endcommentreg = '/^\\s*--\\|>\\s*$/m';
         }
         $comments = '';
         $offset = 0;
