0f547a10a0047f5531d6d3eb972b911b5c42e026
Juan Carlos Rodríguez del Pino <jc.rodriguezdelpino@ulpgc.es>
2024-02-24 11:22:35 +0000

    Set NODE_PATH allowing using global modules

--- jail/default_scripts/javascript_run.sh
+++ jail/default_scripts/javascript_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This file is part of VPL for Moodle - http://vpl.dis.ulpgc.es/
 # Script for JavaScript language using NodeJs
-# Copyright (C) 2014 Juan Carlos Rodríguez-del-Pino
+# Copyright (C) 2024 Juan Carlos Rodríguez-del-Pino
 # License http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
 # Author Juan Carlos Rodríguez-del-Pino <jcrodriguez@dis.ulpgc.es>
 
@@ -12,7 +12,9 @@ check_program nodejs
 if [ "$1" == "version" ] ; then
 	get_program_version -v
 fi
+
 get_first_source_file js
 cat common_script.sh > vpl_execution
+[ "$(command -pv npm)" != "" ] && (echo "export NODE_PATH=$(npm root -g)" >> vpl_execution)
 echo "nodejs \"$FIRST_SOURCE_FILE\" \$@" >> vpl_execution
 chmod +x vpl_execution
