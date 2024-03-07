f4c2fdb043865adaad5539c72e4777329e4605c7
Astor Bizard <13221286+Astor-Bizard@users.noreply.github.com>
2024-03-07 11:00:49 +0100

    Allow whitespaces before single line comments

--- vpl_submission.class.php
+++ vpl_submission.class.php
@@ -936,10 +936,10 @@ class mod_vpl_submission {
     public static function find_proposedcomment(&$text) {
         $usecrnl = vpl_detect_newline($text) == "\r\n";
         if ($usecrnl) {
-            $startcommentreg = '/^(Comment :=>>([^\\r\\n]*)|\\s*<\\|--\\s*)\\r?$/m';
+            $startcommentreg = '/^\\s*(Comment :=>>([^\\r\\n]*)|<\\|--)\\s*\\r?$/m';
             $endcommentreg = '/^\\s*--\\|>\\s*\\r?$/m';
         } else {
-            $startcommentreg = '/^(Comment :=>>(.*)|\\s*<\\|--\\s*)$/m';
+            $startcommentreg = '/^\\s*(Comment :=>>(.*)|<\\|--)\\s*$/m';
             $endcommentreg = '/^\\s*--\\|>\\s*$/m';
         }
         $comments = '';
@@ -949,7 +949,7 @@ class mod_vpl_submission {
             $result = preg_match($startcommentreg, $text, $matches, PREG_OFFSET_CAPTURE, $offset);
             if ( $result == 1) {
                 $found = $matches[1][0];
-                if ( trim($found) == self::BEGINCOMMENTTAG ) { // Block comment start.
+                if ( $found == self::BEGINCOMMENTTAG ) { // Block comment start.
                     $posstart = $matches[0][1] + strlen($matches[0][0]) + 1;
                     $result = preg_match($endcommentreg, $text, $matches, PREG_OFFSET_CAPTURE, $posstart);
                     if ($result == 1) { // Block comment end.
