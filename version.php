31b9f7d50e51b5359e8ed857f868c91672ec1461
Juan Carlos Rodríguez-del-Pino <jc.rodriguezdelpino@ulpgc.es>
2024-05-21 10:00:04 +0100

    Update version

--- version.php
+++ version.php
@@ -29,8 +29,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2024011312;
+$plugin->version = 2024011314;
 $plugin->requires = 2018051713; // Moodle 3.5!
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '4.2.3';
+$plugin->release = '4.3.0';
 $plugin->component = 'mod_vpl';
