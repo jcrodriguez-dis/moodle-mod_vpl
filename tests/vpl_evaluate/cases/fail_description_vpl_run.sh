6cc153350be4f3fad65c6025a293c03082246f50
Juan Carlos Rodríguez-del-Pino <jc.rodriguezdelpino@ulpgc.es>
2025-02-26 16:06:35 +0000

    Removes unneeded files due directory name change

--- tests/vpl_evaluate/cases/fail_description_vpl_run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cat > vpl_execution << "ENDOFSCRIPT"
-#!/bin/bash
-read A
-echo "$A"
-ENDOFSCRIPT
-chmod +x vpl_execution
--- tests/vpl_evaluate/cases/input_vpl_test_evaluate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-if [ -s "$VPLTESTERRORS" ] ; then
-    exit 1
-fi
-grep -e "Grade :=>>10$" "$VPLTESTOUTPUT" >/dev/null
