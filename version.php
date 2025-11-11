31101fd1507157ac7e0119473fa7bdbf1a0aa88c
Juan Carlos Rodríguez del Pino <jc.rodriguezdelpino@ulpgc.es>
2025-11-11 13:49:11 +0000

    Update version number and release status to 4.4.0

--- version.php
+++ version.php
@@ -29,8 +29,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025102910;
+$plugin->version = 2025111111;
 $plugin->requires = 2020061525.00; // Moodle 3.9!
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '4.4.0 RC1';
+$plugin->release = '4.4.0';
 $plugin->component = 'mod_vpl';
